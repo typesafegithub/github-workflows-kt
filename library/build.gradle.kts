8766dc5e496feb2ff9956af6ad5605b3dd8b5358
Piotr Krzeminski <git@krzeminski.it>
2022-01-16 07:54:46 +0100

    [#8] Put library in dedicated module
    We don't want to publish the examples to Maven Central, hence such
    division.

---
+++ library/build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+}
+
+group = "it.krzeminski"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.charleskorn.kaml:kaml:0.38.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
