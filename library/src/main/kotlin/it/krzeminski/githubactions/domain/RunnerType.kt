42a4bc5e3950a4721f600ac6f3189c728c637514
Harsh Shandilya <me@msfjarvis.dev>
2022-01-21 20:04:42 +0530

    Add all missing GitHub hosted runner types

--- library/src/main/kotlin/it/krzeminski/githubactions/domain/RunnerType.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/domain/RunnerType.kt
@@ -1,5 +1,25 @@
 package it.krzeminski.githubactions.domain
 
+/**
+ * Types of GitHub-hosted runners available. Should be kept in sync with the official list at
+ * https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#choosing-github-hosted-runners
+ */
 enum class RunnerType {
+    // "Latest" labels
     UbuntuLatest,
+    WindowsLatest,
+    MacOSLatest,
+
+    // Windows runners
+    Windows2022,
+    Windows2019,
+    Windows2016,
+
+    // Ubuntu runners
+    Ubuntu2004,
+    Ubuntu1804,
+
+    // macOS runners
+    MacOS11,
+    MacOS1015,
 }
