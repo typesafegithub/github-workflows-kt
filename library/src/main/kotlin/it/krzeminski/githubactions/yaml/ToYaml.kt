97d0dd82a08d92e8de3acc32ca9bac0be21fe6ae
Harsh Shandilya <me@msfjarvis.dev>
2022-01-21 20:04:42 +0530

    Add all missing GitHub hosted runner types

--- library/src/main/kotlin/it/krzeminski/githubactions/yaml/ToYaml.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/yaml/ToYaml.kt
@@ -7,7 +7,16 @@ import it.krzeminski.githubactions.domain.CommandStep
 import it.krzeminski.githubactions.domain.ExternalActionStep
 import it.krzeminski.githubactions.domain.Job
 import it.krzeminski.githubactions.domain.RunnerType
+import it.krzeminski.githubactions.domain.RunnerType.MacOS1015
+import it.krzeminski.githubactions.domain.RunnerType.MacOS11
+import it.krzeminski.githubactions.domain.RunnerType.MacOSLatest
+import it.krzeminski.githubactions.domain.RunnerType.Ubuntu1804
+import it.krzeminski.githubactions.domain.RunnerType.Ubuntu2004
 import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
+import it.krzeminski.githubactions.domain.RunnerType.Windows2016
+import it.krzeminski.githubactions.domain.RunnerType.Windows2019
+import it.krzeminski.githubactions.domain.RunnerType.Windows2022
+import it.krzeminski.githubactions.domain.RunnerType.WindowsLatest
 import it.krzeminski.githubactions.domain.Step
 import it.krzeminski.githubactions.domain.Trigger
 import it.krzeminski.githubactions.domain.Workflow
@@ -112,4 +121,13 @@ fun List<Step>.toYaml() =
 fun RunnerType.toYaml() =
     when (this) {
         UbuntuLatest -> "ubuntu-latest"
+        WindowsLatest -> "windows-latest"
+        MacOSLatest -> "macos-latest"
+        Windows2022 -> "windows-2022"
+        Windows2019 -> "windows-2019"
+        Windows2016 -> "windows-2016"
+        Ubuntu2004 -> "ubuntu-20.04"
+        Ubuntu1804 -> "ubuntu-18.04"
+        MacOS11 -> "macos-11"
+        MacOS1015 -> "macos-10.15"
     }
