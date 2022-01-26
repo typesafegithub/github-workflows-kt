ffdba1d926dc5d213fc1b3eca248802cc10bc3e6
Piotr Krzeminski <git@krzeminski.it>
2022-01-26 12:57:50 +0100

    [#48] Add missing versions to actions

--- library/src/main/kotlin/it/krzeminski/githubactions/actions/gradle/WrapperValidationActionV1.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/actions/gradle/WrapperValidationActionV1.kt
@@ -2,6 +2,6 @@ package it.krzeminski.githubactions.actions.gradle
 
 import it.krzeminski.githubactions.actions.Action
 
-class WrapperValidationActionV1 : Action("gradle/wrapper-validation-action") {
+class WrapperValidationActionV1 : Action("gradle/wrapper-validation-action@v1") {
     override fun toYamlArguments() = linkedMapOf<String, String>()
 }
