17bbe76c19c4914a10ead6985db57b5518090d60
Piotr Krzeminski <git@krzeminski.it>
2022-01-27 09:47:00 +0100

    Specify Kotlin version once
    Before this change, it produced a build-time warning.

--- action-validation-tests/build.gradle.kts
+++ action-validation-tests/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm")
     kotlin("plugin.serialization") version "1.6.10"
 }
 
---
+++ build.gradle.kts
@@ -0,0 +1,3 @@
+plugins {
+    kotlin("jvm") version "1.6.10" apply false
+}
