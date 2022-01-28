36e38029fee9d7e48bc1f4afce3aab3639d20f7b
Piotr Krzeminski <git@krzeminski.it>
2022-01-28 07:55:55 +0100

    [#47] Add missing parameters for 'gradle-update/update-gradle-wrapper-action@v1'

--- library/src/main/kotlin/it/krzeminski/githubactions/actions/gradleupdate/UpdateGradleWrapperActionV1.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/actions/gradleupdate/UpdateGradleWrapperActionV1.kt
@@ -2,6 +2,30 @@ package it.krzeminski.githubactions.actions.gradleupdate
 
 import it.krzeminski.githubactions.actions.Action
 
-class UpdateGradleWrapperActionV1 : Action("gradle-update", "update-gradle-wrapper-action", "v1") {
-    override fun toYamlArguments() = linkedMapOf<String, String>()
+@Suppress("LongParameterList")
+class UpdateGradleWrapperActionV1(
+    val repoToken: String? = null,
+    val reviewers: List<String>? = null,
+    val teamReviewers: List<String>? = null,
+    val labels: List<String>? = null,
+    val baseBranch: String? = null,
+    val targetBranch: String? = null,
+    val setDistributionChecksum: Boolean? = null,
+    val paths: List<String>? = null,
+    val pathsIgnore: List<String>? = null,
+) : Action("gradle-update", "update-gradle-wrapper-action", "v1") {
+    @Suppress("SpreadOperator")
+    override fun toYamlArguments() = linkedMapOf(
+        *listOfNotNull(
+            repoToken?.let { "repo-token" to it },
+            reviewers?.let { "reviewers" to it.joinToString(",") },
+            teamReviewers?.let { "team-reviewers" to it.joinToString(",") },
+            labels?.let { "labels" to it.joinToString(",") },
+            baseBranch?.let { "base-branch" to it },
+            targetBranch?.let { "target-branch" to it },
+            setDistributionChecksum?.let { "set-distribution-checksum" to it.toString() },
+            paths?.let { "paths" to it.joinToString(",") },
+            pathsIgnore?.let { "paths-ignore" to it.joinToString(",") },
+        ).toTypedArray()
+    )
 }
--- library/src/test/kotlin/it/krzeminski/githubactions/actions/gradleupdate/UpdateGradleWrapperActionV1Test.kt
+++ library/src/test/kotlin/it/krzeminski/githubactions/actions/gradleupdate/UpdateGradleWrapperActionV1Test.kt
@@ -14,4 +14,35 @@ class UpdateGradleWrapperActionV1Test : DescribeSpec({
         // then
         yaml shouldBe linkedMapOf()
     }
+
+    it("renders with all parameters") {
+        // given
+        val action = UpdateGradleWrapperActionV1(
+            repoToken = "repo-token1",
+            reviewers = listOf("reviewer1", "reviewer2"),
+            teamReviewers = listOf("team-reviewer1", "team-reviewer2"),
+            labels = listOf("label1", "label2"),
+            baseBranch = "base-branch1",
+            targetBranch = "target-branch1",
+            setDistributionChecksum = false,
+            paths = listOf("path1", "path2"),
+            pathsIgnore = listOf("path-ignore1", "path-ignore2"),
+        )
+
+        // when
+        val yaml = action.toYamlArguments()
+
+        // then
+        yaml shouldBe linkedMapOf(
+            "repo-token" to "repo-token1",
+            "reviewers" to "reviewer1,reviewer2",
+            "team-reviewers" to "team-reviewer1,team-reviewer2",
+            "labels" to "label1,label2",
+            "base-branch" to "base-branch1",
+            "target-branch" to "target-branch1",
+            "set-distribution-checksum" to "false",
+            "paths" to "path1,path2",
+            "paths-ignore" to "path-ignore1,path-ignore2",
+        )
+    }
 })
