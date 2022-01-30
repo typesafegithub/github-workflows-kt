594ce1668ff992b5231b79b9be0dbc4da8e8d051
Jean-Michel Fayard <jmfayard@gmail.com>
2022-01-30 18:51:26 +0100

    Add type-safe Cron() declaration

--- library/src/main/kotlin/it/krzeminski/githubactions/domain/Trigger.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/domain/Trigger.kt
@@ -9,4 +9,53 @@ sealed class Trigger {
     ) : Trigger()
 }
 
+/** See https://crontab.guru **/
 data class Cron(val expression: String)
+
+/** See https://crontab.guru **/
+fun Cron(
+    minute: String = "*",
+    hour: String = "*",
+    dayMonth: String = "*",
+    month: String = "*",
+    dayWeek: String = "*",
+): Cron {
+    @Suppress("MagicNumber")
+    val errors = listOf(
+        checkCronSyntax(minute, "minute", 0..59),
+        checkCronSyntax(hour, "hour", 0..23),
+        checkCronSyntax(dayMonth, "dayMonth", 0..59),
+        checkCronSyntax(month, "month", 1..31),
+        checkCronSyntax(dayWeek, "dayWeek", 0..6),
+    ).flatten()
+    val cron = "$minute $hour $dayMonth $month $dayWeek"
+    check(errors.isEmpty()) {
+        val formattedErrors = errors.joinToString(separator = "\n- ", prefix = "\n- ")
+        "Your Cron syntax [$cron] contains errors:$formattedErrors"
+    }
+    return Cron(cron)
+}
+
+private fun checkCronSyntax(value: String, field: String, range: IntRange): List<String> {
+    val errors = mutableListOf<String>()
+    fun addErrorIf(message: String, predicate: () -> Boolean) {
+        if (predicate()) errors += "Field '$field' with value [$value] $message"
+    }
+
+    addErrorIf("contains invalid characters") {
+        val allowedCharacters = ('0'..'9') + listOf('-', '*', ',', '/')
+        value.any { it !in allowedCharacters }
+    }
+    addErrorIf("contains number(s) outside of range $range") {
+        val replace = value.replace(Regex("[-*,/]"), " ")
+        val numbers = replace
+            .split(" ")
+            .mapNotNull { it.toIntOrNull() }
+        numbers.any { it !in range }
+    }
+    addErrorIf("is blank") {
+        value.isBlank()
+    }
+
+    return errors
+}
