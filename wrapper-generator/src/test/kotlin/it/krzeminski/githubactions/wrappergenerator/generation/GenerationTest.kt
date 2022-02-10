6dbaab0081cc4b22f41c8f5a648f9e6ecc7ce7c1
Piotr Krzeminski <git@krzeminski.it>
2022-02-10 06:54:53 +0100

    [#71] Add tests for wrapper generation

---
+++ wrapper-generator/src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/generation/GenerationTest.kt
@@ -0,0 +1,70 @@
+package it.krzeminski.githubactions.wrappergenerator.generation
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import it.krzeminski.githubactions.wrappergenerator.domain.ActionCoords
+import it.krzeminski.githubactions.wrappergenerator.metadata.Input
+import it.krzeminski.githubactions.wrappergenerator.metadata.Metadata
+
+class GenerationTest : FunSpec({
+    test("action with required inputs as strings, no outputs") {
+        // given
+        val actionManifest = Metadata(
+            name = "Do something cool",
+            description = "This is a test description that should be put in the KDoc comment for a class",
+            inputs = mapOf(
+                "foo-bar" to Input(
+                    description = "Short description",
+                    default = null,
+                ),
+                "baz-goo" to Input(
+                    description = "Just another input",
+                    default = null,
+                ),
+            )
+        )
+        val coords = ActionCoords("john-smith", "do-sth-cool", "v3")
+        val fetchMetadataMock = mockk<ActionCoords.() -> Metadata>()
+        every { fetchMetadataMock(any()) } returns actionManifest
+
+        // when
+        val wrapper = coords.generateWrapper(fetchMetadataMock)
+
+        // then
+        wrapper shouldBe Wrapper(
+            kotlinCode = """
+                package it.krzeminski.githubactions.actions.johnsmith
+
+                import it.krzeminski.githubactions.actions.Action
+                import kotlin.String
+
+                /**
+                 * Action: Do something cool
+                 *
+                 * This is a test description that should be put in the KDoc comment for a class
+                 *
+                 * http://github.com/john-smith/do-sth-cool
+                 */
+                public class DoSthCoolV3(
+                    /**
+                     * Short description
+                     */
+                    fooBar: String,
+                    /**
+                     * Just another input
+                     */
+                    bazGoo: String
+                ) : Action("john-smith", "do-sth-cool", "v3") {
+                    public override fun toYamlArguments() = linkedMapOf(
+                        "foo-bar" to fooBar,
+                        "baz-goo" to bazGoo,
+                    )
+                }
+
+            """.trimIndent(),
+            filePath = "library/src/main/kotlin/it/krzeminski/githubactions/actions/johnsmith/DoSthCoolV3.kt",
+        )
+    }
+})
---
+++ wrapper-generator/src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/generation/TextTransformationTest.kt
@@ -0,0 +1,36 @@
+package it.krzeminski.githubactions.wrappergenerator.generation
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class TextTransformationTest : FunSpec({
+    context("toKotlinPackageName") {
+        listOf(
+            "some-name" to "somename",
+        ).forEach { (input, output) ->
+            test("should convert '$input' to '$output'") {
+                input.toKotlinPackageName() shouldBe output
+            }
+        }
+    }
+
+    context("toPascalCase") {
+        listOf(
+            "some-name" to "SomeName",
+        ).forEach { (input, output) ->
+            test("should convert '$input' to '$output'") {
+                input.toPascalCase() shouldBe output
+            }
+        }
+    }
+
+    context("toCamelCase") {
+        listOf(
+            "some-name" to "someName",
+        ).forEach { (input, output) ->
+            test("should convert '$input' to '$output'") {
+                input.toCamelCase() shouldBe output
+            }
+        }
+    }
+})
