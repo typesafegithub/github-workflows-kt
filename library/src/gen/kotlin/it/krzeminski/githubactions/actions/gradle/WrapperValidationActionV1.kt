4d250ee9bf9b99d529f928f555717816652f71ac
Piotr Krzeminski <git@krzeminski.it>
2022-02-11 17:03:44 +0100

    [#71] Generate wrapper for gradle/wrapper-validation-action@v1

---
+++ library/src/gen/kotlin/it/krzeminski/githubactions/actions/gradle/WrapperValidationActionV1.kt
@@ -0,0 +1,42 @@
+// This file was generated using 'wrapper-generator' module. Don't change it by hand, your changes will
+// be overwritten with the next wrapper code regeneration. Instead, consider introducing changes to the
+// generator itself.
+package it.krzeminski.githubactions.actions.gradle
+
+import it.krzeminski.githubactions.actions.Action
+import kotlin.Boolean
+import kotlin.Int
+import kotlin.String
+import kotlin.Suppress
+import kotlin.collections.List
+
+/**
+ * Action: Gradle Wrapper Validation
+ *
+ * Validates Gradle Wrapper JAR Files
+ *
+ * https://github.com/gradle/wrapper-validation-action
+ */
+public class WrapperValidationActionV1(
+    /**
+     * Minimum expected wrapper JAR files
+     */
+    public val minWrapperCount: Int? = null,
+    /**
+     * Allow snapshot Gradle versions
+     */
+    public val allowSnapshots: Boolean? = null,
+    /**
+     * Allow arbitrary checksums, comma separated
+     */
+    public val allowChecksums: List<String>? = null
+) : Action("gradle", "wrapper-validation-action", "v1") {
+    @Suppress("SpreadOperator")
+    public override fun toYamlArguments() = linkedMapOf(
+        *listOfNotNull(
+            minWrapperCount?.let { "min-wrapper-count" to it.toString() },
+            allowSnapshots?.let { "allow-snapshots" to it.toString() },
+            allowChecksums?.let { "allow-checksums" to it.joinToString(",") },
+        ).toTypedArray()
+    )
+}
--- wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/WrappersToGenerate.kt
+++ wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/WrappersToGenerate.kt
@@ -3,6 +3,7 @@ package it.krzeminski.githubactions.wrappergenerator
 import it.krzeminski.githubactions.wrappergenerator.domain.ActionCoords
 import it.krzeminski.githubactions.wrappergenerator.domain.WrapperRequest
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.BooleanTyping
+import it.krzeminski.githubactions.wrappergenerator.domain.typings.IntegerTyping
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.ListOfStringsTyping
 
 val wrappersToGenerate = listOf(
@@ -19,6 +20,14 @@ val wrappersToGenerate = listOf(
             "cache-read-only" to BooleanTyping,
             "gradle-home-cache-includes" to ListOfStringsTyping("\\n"),
             "gradle-home-cache-excludes" to ListOfStringsTyping("\\n"),
+        ),
+    ),
+    WrapperRequest(
+        ActionCoords("gradle", "wrapper-validation-action", "v1"),
+        mapOf(
+            "min-wrapper-count" to IntegerTyping,
+            "allow-snapshots" to BooleanTyping,
+            "allow-checksums" to ListOfStringsTyping(","),
         ),
     ),
 
