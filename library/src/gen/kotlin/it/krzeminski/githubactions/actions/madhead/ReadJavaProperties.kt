622c216010c4d6d59d404c973d7acdc6a5d8061f
Piotr Krzeminski <git@krzeminski.it>
2022-02-11 12:08:15 +0100

    [#71] Generate wrapper for madhead/read-java-properties@latest

---
+++ library/src/gen/kotlin/it/krzeminski/githubactions/actions/madhead/ReadJavaProperties.kt
@@ -0,0 +1,45 @@
+// This file was generated using 'wrapper-generator' module. Don't change it by hand, your changes will
+// be overwritten with the next wrapper code regeneration. Instead, consider introducing changes to the
+// generator itself.
+package it.krzeminski.githubactions.actions.madhead
+
+import it.krzeminski.githubactions.actions.Action
+import kotlin.Boolean
+import kotlin.String
+import kotlin.Suppress
+
+/**
+ * Action: read-java-properties
+ *
+ * GitHub Action to read values from Java's .properties files
+ *
+ * https://github.com/madhead/read-java-properties
+ */
+public class ReadJavaProperties(
+    /**
+     * A file to read
+     */
+    public val `file`: String,
+    /**
+     * A property to read from the file
+     */
+    public val `property`: String? = null,
+    /**
+     * A flag to read and output all the properties from the file
+     */
+    public val all: Boolean? = null,
+    /**
+     * A value to return in case of any errors
+     */
+    public val default: String? = null
+) : Action("madhead", "read-java-properties", "latest") {
+    @Suppress("SpreadOperator")
+    public override fun toYamlArguments() = linkedMapOf(
+        *listOfNotNull(
+            "file" to file,
+            property?.let { "property" to it },
+            all?.let { "all" to it.toString() },
+            default?.let { "default" to it },
+        ).toTypedArray()
+    )
+}
--- wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/WrappersToGenerate.kt
+++ wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/WrappersToGenerate.kt
@@ -2,11 +2,13 @@ package it.krzeminski.githubactions.wrappergenerator
 
 import it.krzeminski.githubactions.wrappergenerator.domain.ActionCoords
 import it.krzeminski.githubactions.wrappergenerator.domain.WrapperRequest
+import it.krzeminski.githubactions.wrappergenerator.domain.typings.BooleanTyping
 
 val wrappersToGenerate = listOf(
     WrapperRequest(ActionCoords("actions", "download-artifact", "v2")),
 
     WrapperRequest(ActionCoords("madhead", "check-gradle-version", "v1")),
+    WrapperRequest(ActionCoords("madhead", "read-java-properties", "latest"), mapOf("all" to BooleanTyping)),
     WrapperRequest(ActionCoords("madhead", "semver-utils", "latest")),
 
     WrapperRequest(ActionCoords("peterjgrainger", "action-create-branch", "v2.1.0")),
