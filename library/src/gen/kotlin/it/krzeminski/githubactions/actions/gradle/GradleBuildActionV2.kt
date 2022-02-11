2b434ee65436e9d2f717966e89c31c26dae7ea2f
Piotr Krzeminski <git@krzeminski.it>
2022-02-11 14:17:45 +0100

    [#71] Generate wrapper for gradle/gradle-build-action@v2

---
+++ library/src/gen/kotlin/it/krzeminski/githubactions/actions/gradle/GradleBuildActionV2.kt
@@ -0,0 +1,84 @@
+// This file was generated using 'wrapper-generator' module. Don't change it by hand, your changes will
+// be overwritten with the next wrapper code regeneration. Instead, consider introducing changes to the
+// generator itself.
+package it.krzeminski.githubactions.actions.gradle
+
+import it.krzeminski.githubactions.actions.Action
+import kotlin.Boolean
+import kotlin.String
+import kotlin.Suppress
+import kotlin.collections.List
+
+/**
+ * Action: Gradle Build Action
+ *
+ * Configures Gradle for use in GitHub actions, caching useful state in the GitHub actions cache
+ *
+ * https://github.com/gradle/gradle-build-action
+ */
+public class GradleBuildActionV2(
+    /**
+     * Gradle version to use
+     */
+    public val gradleVersion: String? = null,
+    /**
+     * When 'true', all caching is disabled. No entries will be written to or read from the cache.
+     */
+    public val cacheDisabled: Boolean? = null,
+    /**
+     * When 'true', existing entries will be read from the cache but no entries will be written.
+     */
+    public val cacheReadOnly: Boolean? = null,
+    /**
+     * Paths within Gradle User Home to cache.
+     */
+    public val gradleHomeCacheIncludes: List<String>? = null,
+    /**
+     * Paths within Gradle User Home to exclude from cache.
+     */
+    public val gradleHomeCacheExcludes: List<String>? = null,
+    /**
+     * Gradle command line arguments (supports multi-line input)
+     */
+    public val arguments: String? = null,
+    /**
+     * Path to the root directory of the build
+     */
+    public val buildRootDirectory: String? = null,
+    /**
+     * Path to the Gradle executable
+     */
+    public val gradleExecutable: String? = null,
+    /**
+     * When 'true', entries will not be restored from the cache but will be saved at the end of the
+     * Job. This allows a 'clean' cache entry to be written.
+     */
+    public val cacheWriteOnly: String? = null,
+    /**
+     * When 'true', the action will not attempt to restore the Gradle User Home entries from other
+     * Jobs.
+     */
+    public val gradleHomeCacheStrictMatch: String? = null,
+    /**
+     * Used to uniquely identify the current job invocation. Defaults to the matrix values for this
+     * job; this should not be overridden by users (INTERNAL).
+     */
+    public val workflowJobContext: String? = null
+) : Action("gradle", "gradle-build-action", "v2") {
+    @Suppress("SpreadOperator")
+    public override fun toYamlArguments() = linkedMapOf(
+        *listOfNotNull(
+            gradleVersion?.let { "gradle-version" to it },
+            cacheDisabled?.let { "cache-disabled" to it.toString() },
+            cacheReadOnly?.let { "cache-read-only" to it.toString() },
+            gradleHomeCacheIncludes?.let { "gradle-home-cache-includes" to it.joinToString("\n") },
+            gradleHomeCacheExcludes?.let { "gradle-home-cache-excludes" to it.joinToString("\n") },
+            arguments?.let { "arguments" to it },
+            buildRootDirectory?.let { "build-root-directory" to it },
+            gradleExecutable?.let { "gradle-executable" to it },
+            cacheWriteOnly?.let { "cache-write-only" to it },
+            gradleHomeCacheStrictMatch?.let { "gradle-home-cache-strict-match" to it },
+            workflowJobContext?.let { "workflow-job-context" to it },
+        ).toTypedArray()
+    )
+}
--- wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/WrappersToGenerate.kt
+++ wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/WrappersToGenerate.kt
@@ -3,6 +3,7 @@ package it.krzeminski.githubactions.wrappergenerator
 import it.krzeminski.githubactions.wrappergenerator.domain.ActionCoords
 import it.krzeminski.githubactions.wrappergenerator.domain.WrapperRequest
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.BooleanTyping
+import it.krzeminski.githubactions.wrappergenerator.domain.typings.ListOfStringsTyping
 
 val wrappersToGenerate = listOf(
     WrapperRequest(ActionCoords("actions", "download-artifact", "v2")),
@@ -10,6 +11,16 @@ val wrappersToGenerate = listOf(
     WrapperRequest(ActionCoords("madhead", "check-gradle-version", "v1")),
     WrapperRequest(ActionCoords("madhead", "read-java-properties", "latest"), mapOf("all" to BooleanTyping)),
     WrapperRequest(ActionCoords("madhead", "semver-utils", "latest")),
+
+    WrapperRequest(
+        ActionCoords("gradle", "gradle-build-action", "v2"),
+        mapOf(
+            "cache-disabled" to BooleanTyping,
+            "cache-read-only" to BooleanTyping,
+            "gradle-home-cache-includes" to ListOfStringsTyping("\\n"),
+            "gradle-home-cache-excludes" to ListOfStringsTyping("\\n"),
+        ),
+    ),
 
     WrapperRequest(ActionCoords("peterjgrainger", "action-create-branch", "v2.1.0")),
 )
