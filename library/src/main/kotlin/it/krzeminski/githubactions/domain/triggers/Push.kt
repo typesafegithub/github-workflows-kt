2cb43180a0c6c38432dd6db6dd56ef429389cd42
Piotr Krzeminski <git@krzeminski.it>
2022-02-19 21:02:04 +0100

    [#1] Unify order of arguments in Push trigger

--- library/src/main/kotlin/it/krzeminski/githubactions/domain/triggers/Push.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/domain/triggers/Push.kt
@@ -2,8 +2,8 @@ package it.krzeminski.githubactions.domain.triggers
 
 data class Push(
     val branches: List<String>? = null,
-    val tags: List<String>? = null,
     val branchesIgnore: List<String>? = null,
+    val tags: List<String>? = null,
     val tagsIgnore: List<String>? = null,
     val paths: List<String>? = null,
     val pathsIgnore: List<String>? = null,
