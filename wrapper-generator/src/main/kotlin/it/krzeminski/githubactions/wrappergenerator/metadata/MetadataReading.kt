27036886946a5f3470068870f4ce1ca5525098f4
Jean-Michel Fayard <jmfayard@gmail.com>
2022-02-23 14:57:07 +0100

    feat(generator): Be more robust while parsing action.yml

--- wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/metadata/MetadataReading.kt
+++ wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/metadata/MetadataReading.kt
@@ -14,13 +14,13 @@ import java.net.URI
 data class Metadata(
     val name: String,
     val description: String,
-    val inputs: Map<String, Input>,
+    val inputs: Map<String, Input> = emptyMap(),
     val outputs: Map<String, Output> = emptyMap(),
 )
 
 @Serializable
 data class Input(
-    val description: String,
+    val description: String = "",
     val default: String? = null,
     val required: Boolean? = null,
     val deprecationMessage: String? = null,
