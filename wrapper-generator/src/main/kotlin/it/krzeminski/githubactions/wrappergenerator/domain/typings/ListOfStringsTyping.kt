59edefcf77ea6db137047567ea6b2a4a6f122444
Jean-Michel Fayard <jmfayard@gmail.com>
2022-02-23 14:53:09 +0100

    feat(generator): ListOfStringsTyping("\n") is invalid

--- wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/domain/typings/ListOfStringsTyping.kt
+++ wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/domain/typings/ListOfStringsTyping.kt
@@ -6,6 +6,9 @@ import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.asClassName
 
 class ListOfStringsTyping(private val delimiter: String) : Typing {
+    init {
+        require(delimiter != "\n") { """ListOfStringsTyping(newline) invalid, use ListOfStringsTyping("\\n") instead"""}
+    }
     override fun getClassName(actionPackageName: String, actionClassName: String): TypeName =
         List::class.asClassName().parameterizedBy(ClassName("kotlin", "String"))
 
