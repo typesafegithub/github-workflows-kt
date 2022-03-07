56e61d9435d2776d9d53fa72e1ac2033c7db8da1
Jean-Michel Fayard <jmfayard@gmail.com>
2022-03-07 12:33:25 +0100

    build: enable Gradle Build Scan
    So that we can see the test failures on GitHub Actions instead of this:
    
    https://github.com/krzema12/github-actions-kotlin-dsl/runs/5437463116?check_suite_focus=true

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
+plugins {
+    id("com.gradle.enterprise").version("3.8.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
+}
+
 rootProject.name = "github-actions-kotlin-dsl"
 
 include(
