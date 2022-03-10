74666ea73712b3bbcf147d24a9bb455652d5bd12
Piotr Krzeminski <git@krzeminski.it>
2022-03-10 09:21:06 +0100

    [#116] chore: publish Gradle build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,16 @@ include(
     "library",
     "wrapper-generator",
 )
+
+plugins {
+    id("com.gradle.enterprise").version("3.8.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
+}
