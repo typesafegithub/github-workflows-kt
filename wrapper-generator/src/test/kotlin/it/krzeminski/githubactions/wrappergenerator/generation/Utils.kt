100e0b9f62f49641bdc7d76e0c8daadcad206649
Jean-Michel Fayard <jmfayard@gmail.com>
2022-03-17 22:55:53 +0100

    tests: compare GeneratedFile.kt with GeneratedFileActual.kt
    When a generated file doesn't match what was expected,
    the actual content is written to a file GeneratedFileActual.kt
    
    Use IntelliJ's "Compare Files" Actions
    and update GeneratedFile.kt if the change looks good
    
    Next time the test run and match the expected content,
    GeneratedFileActual.kt will be deleted
    
    On GitHub Actions, fail instead of creating a new file

--- wrapper-generator/src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/generation/Utils.kt
+++ wrapper-generator/src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/generation/Utils.kt
@@ -1,8 +1,29 @@
 package it.krzeminski.githubactions.wrappergenerator.generation
 
+import io.kotest.assertions.fail
+import io.kotest.matchers.shouldBe
 import java.nio.file.Paths
 
-fun writeToUnitTests(wrapper: Wrapper) {
-    Paths.get("src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/generation/wrappersfromunittests/${Paths.get(wrapper.filePath).fileName}")
-        .toFile().writeText(wrapper.kotlinCode)
+fun Wrapper.shouldMatchFile(path: String) {
+    val expectedFile =
+        Paths.get("src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/generation/wrappersfromunittests/$path")
+            .toFile()
+    val actualFile = expectedFile.resolveSibling(expectedFile.nameWithoutExtension + "Actual.kt")
+    val expected = if (expectedFile.canRead()) expectedFile.readText() else ""
+
+    filePath shouldBe "library/src/gen/kotlin/it/krzeminski/githubactions/actions/johnsmith/$path"
+
+    if (System.getenv("GITHUB_ACTIONS") == "true") {
+        kotlinCode shouldBe expected
+    } else if (kotlinCode == expected) {
+        actualFile.delete()
+    } else {
+        actualFile.writeText(
+            kotlinCode.replace(
+                "package it.krzeminski.githubactions.actions.johnsmith",
+                "package it.krzeminski.githubactions.actions.actual"
+            )
+        )
+        fail("The Wrapper's kotlin code in ${actualFile.name} doesn't match ${expectedFile.name}\nSee folder ${expectedFile.parentFile.canonicalPath}")
+    }
 }
