002d2f4649774adbd82a55bd8e4996c7fe92ab71
Jean-Michel Fayard <jmfayard@gmail.com>
2022-03-19 10:05:09 +0100

    pr: prevent the expected file to contain the expected package

--- wrapper-generator/src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/generation/Utils.kt
+++ wrapper-generator/src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/generation/Utils.kt
@@ -2,6 +2,8 @@ package it.krzeminski.githubactions.wrappergenerator.generation
 
 import io.kotest.assertions.fail
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNot
+import io.kotest.matchers.string.contain
 import java.nio.file.Paths
 
 fun Wrapper.shouldMatchFile(path: String) {
@@ -17,16 +19,17 @@ fun Wrapper.shouldMatchFile(path: String) {
 
     filePath shouldBe "library/src/gen/kotlin/it/krzeminski/githubactions/actions/johnsmith/$path"
 
+    val packageName = "it.krzeminski.githubactions.actions.johnsmith"
+    expectedContent shouldNot contain("package $packageName.actual")
+
     if (System.getenv("GITHUB_ACTIONS") == "true") {
         actualContent shouldBe expectedContent
     } else if (actualContent == expectedContent) {
         actualFile.delete()
     } else {
         actualFile.writeText(
-            actualContent.replace(
-                "package it.krzeminski.githubactions.actions.johnsmith",
-                "package it.krzeminski.githubactions.actions.actual"
-            )
+            // change the package to avoid compilation errors because of duplicate classes / functions
+            actualContent.replace("package $packageName", "package $packageName.actual")
         )
         fail("The Wrapper's kotlin code in ${actualFile.name} doesn't match ${expectedFile.name}\nSee folder ${expectedFile.parentFile.canonicalPath}")
     }
