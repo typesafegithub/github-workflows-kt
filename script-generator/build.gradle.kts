9fbb92c5b0efbe42309db3f9627d70dfb95f8484
Jean-Michel Fayard <jmfayard@gmail.com>
2022-03-24 11:19:46 +0100

    feat(script-generator): initialize module

---
+++ script-generator/build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization") version "1.6.10"
+    application
+
+    // Code quality.
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":wrapper-generator"))
+    implementation(project(":library"))
+    implementation("com.charleskorn.kaml:kaml:0.42.0")
+    implementation("com.squareup:kotlinpoet:1.10.2")
+
+    testImplementation(project(":library"))
+    testImplementation("io.kotest:kotest-assertions-core:5.1.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.1.0")
+    testImplementation(project(":library"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("it.krzeminski.githubactions.scriptgenerator.MainKt")
+    tasks.run.get().workingDir = rootProject.projectDir
+}
+
+tasks.getByName("run") {
+    finalizedBy(":library:ktlintFormat")
+}
+
+configure<KtlintExtension> {
+    filter {
+        exclude("**/generated/**", "**/actual/**")
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+        freeCompilerArgs += listOf(
+            "-Xopt-in=kotlin.RequiresOptIn",
+            "-Xopt-in=it.krzeminski.githubactions.internal.InternalGithubActionsApi"
+        )
+    }
+}
