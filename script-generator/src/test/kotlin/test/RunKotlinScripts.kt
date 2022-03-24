8477ddd764cde6f886a82e25ca1bf97bc8bda483
Jean-Michel Fayard <jmfayard@gmail.com>
2022-03-24 11:28:24 +0100

    feat(script-generator): integration tests
    See `script-generator/README.md`

---
+++ script-generator/src/test/kotlin/test/RunKotlinScripts.kt
@@ -0,0 +1,31 @@
+package test
+
+import generated.workflowAllTriggers
+import generated.workflowDockerImage
+import generated.workflowGenerateWrappers
+import generated.workflowGenerated
+import generated.workflowNodejsPackage
+import generated.workflowRefreshversionsBuild
+import generated.workflowRefreshversionsPr
+import generated.workflowRefreshversionsWebsite
+import generated.workflowUpdateGradleWrapper
+import io.kotest.core.spec.style.FunSpec
+import it.krzeminski.githubactions.yaml.writeToFile
+
+class RunKotlinScripts : FunSpec({
+    val allWorkflows = listOf(
+        workflowGenerateWrappers,
+        workflowRefreshversionsBuild,
+        workflowAllTriggers,
+        workflowGenerated,
+        workflowRefreshversionsPr,
+        workflowRefreshversionsWebsite,
+        workflowUpdateGradleWrapper,
+        workflowNodejsPackage,
+        workflowDockerImage,
+    )
+
+    test("Execute Kotlin Scripts") {
+        allWorkflows.forEach { it.writeToFile(addConsistencyCheck = false) }
+    }
+})
