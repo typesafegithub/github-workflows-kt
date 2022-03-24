b4e16963cb493d46b640335f05dfa9266c098ea0
Jean-Michel Fayard <jmfayard@gmail.com>
2022-03-24 11:01:08 +0100

    feat(script-generator): minor change to the library module

--- library/src/main/kotlin/it/krzeminski/githubactions/yaml/Case.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/yaml/Case.kt
@@ -1,11 +1,19 @@
 package it.krzeminski.githubactions.yaml
 
-inline fun <reified T : Enum<T>> T.toSnakeCase(): String {
+import it.krzeminski.githubactions.internal.InternalGithubActionsApi
+
+@InternalGithubActionsApi
+inline fun <reified T : Enum<T>> T.toSnakeCase(): String =
+    snakeCaseOf(name)
+
+@InternalGithubActionsApi
+fun snakeCaseOf(name: String): String {
     require(name.first() !in 'a'..'z' && name.all { it in 'a'..'z' || it in 'A'..'Z' })
     return pascalCaseRegex.findAll(name).joinToString(separator = "_") { it.value.lowercase() }
 }
 
+@InternalGithubActionsApi
 inline fun <reified T : Enum<T>> List<T>.toSnakeCase(): List<String> =
     this.map { it.toSnakeCase() }
 
-val pascalCaseRegex = Regex("[A-Z][a-z]*")
+private val pascalCaseRegex = Regex("[A-Z][a-z]*")
