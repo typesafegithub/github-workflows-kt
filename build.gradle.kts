534523d67048c69a6fe28de36b5428d368531450
Piotr Krzeminski <git@krzeminski.it>
2022-04-22 23:30:16 +0200

    [#151] chore(ci): release to Maven Central on version tag push

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,14 @@ plugins {
     // Code quality.
     id("io.gitlab.arturbosch.detekt") version "1.20.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1" apply false
+
+    // Publishing.
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0" // Needs to be applied to the root project.
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+    packageGroup.set("it.krzeminski")
 }
