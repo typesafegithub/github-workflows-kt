975dd7dd4aba73e6eb735bf6effa7f8d13b022d8
Piotr Krzeminski <git@krzeminski.it>
2022-04-24 22:42:20 +0200

    [#218] feat(library): support custom arguments for steps

--- library/src/main/kotlin/it/krzeminski/githubactions/domain/Step.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/domain/Step.kt
@@ -2,12 +2,15 @@ package it.krzeminski.githubactions.domain
 
 import it.krzeminski.githubactions.actions.Action
 import it.krzeminski.githubactions.actions.ActionWithOutputs
+import it.krzeminski.githubactions.dsl.CustomValue
+import it.krzeminski.githubactions.dsl.HasCustomArguments
 
 sealed class Step(
     open val id: String,
     open val env: LinkedHashMap<String, String> = linkedMapOf(),
     open val condition: String? = null,
-)
+    override val _customArguments: Map<String, CustomValue> = emptyMap(),
+) : HasCustomArguments
 
 interface WithOutputs<T> {
     val outputs: T
@@ -19,10 +22,12 @@ data class CommandStep(
     val command: String,
     override val env: LinkedHashMap<String, String> = linkedMapOf(),
     override val condition: String? = null,
+    override val _customArguments: Map<String, CustomValue> = emptyMap(),
 ) : Step(
     id = id,
     condition = condition,
     env = env,
+    _customArguments = _customArguments,
 )
 
 open class ExternalActionStep(
@@ -31,10 +36,12 @@ open class ExternalActionStep(
     open val action: Action,
     override val env: LinkedHashMap<String, String> = linkedMapOf(),
     override val condition: String? = null,
+    override val _customArguments: Map<String, CustomValue> = emptyMap(),
 ) : Step(
     id = id,
     condition = condition,
     env = env,
+    _customArguments = _customArguments,
 )
 
 data class ExternalActionStepWithOutputs<T>(
@@ -44,11 +51,13 @@ data class ExternalActionStepWithOutputs<T>(
     override val env: LinkedHashMap<String, String> = linkedMapOf(),
     override val condition: String? = null,
     override val outputs: T,
+    override val _customArguments: Map<String, CustomValue> = emptyMap(),
 ) : ExternalActionStep(
     name = name,
     action = action,
     id = id,
     condition = condition,
     env = env,
+    _customArguments = _customArguments,
 ),
     WithOutputs<T>
