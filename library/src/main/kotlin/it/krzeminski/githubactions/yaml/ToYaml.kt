f618aa339ac6ff3d51b5c0d7d6ffd199d46020dc
Piotr Krzeminski <git@krzeminski.it>
2022-04-24 22:31:24 +0200

    [#216] fix(library): add trailing empty line to generated YAML
    It makes YAML diffs cleaner if something's appended at the end.

--- library/src/main/kotlin/it/krzeminski/githubactions/yaml/ToYaml.kt
+++ library/src/main/kotlin/it/krzeminski/githubactions/yaml/ToYaml.kt
@@ -94,6 +94,8 @@ private fun Workflow.generateYaml(addConsistencyCheck: Boolean, useGitDiff: Bool
                 append("\n")
                 append(freeargs.replaceIndent(""))
             }
+
+        appendLine()
     }.also { yamlContent ->
         failIfMalformedYml(yamlContent)
     }
