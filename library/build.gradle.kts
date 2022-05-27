014165dd8ec6b7a767c0d9cdb05eeaf6b87d0d22
Piotr Krzeminski <git@krzeminski.it>
2022-05-27 20:41:07 +0200

    chore: bump version to 0.17.0

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 }
 
 group = "it.krzeminski"
-version = "0.16.0"
+version = "0.17.0"
 
 repositories {
     mavenCentral()
--- script-generator/src/main/kotlin/it/krzeminski/githubactions/scriptgenerator/Version.kt
+++ script-generator/src/main/kotlin/it/krzeminski/githubactions/scriptgenerator/Version.kt
@@ -1,3 +1,3 @@
 package it.krzeminski.githubactions.scriptgenerator
 
-val LIBRARY_VERSION = "0.16.0"
+val LIBRARY_VERSION = "0.17.0"
