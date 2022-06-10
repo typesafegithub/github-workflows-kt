0594f842a40a5f5a48cccbe86b6e51c91f227f57
Piotr Krzemiński <git@krzeminski.it>
2022-06-10 09:56:26 +0200

    [#261] feat(wrappers): add actions/setup-python@v4

--- library/src/gen/kotlin/it/krzeminski/githubactions/actions/actions/SetupPythonV4.kt
+++ library/src/gen/kotlin/it/krzeminski/githubactions/actions/actions/SetupPythonV4.kt
@@ -1,12 +1,9 @@
 // This file was generated using 'wrapper-generator' module. Don't change it by hand, your changes will
 // be overwritten with the next wrapper code regeneration. Instead, consider introducing changes to the
 // generator itself.
-@file:Suppress("DEPRECATION")
-
 package it.krzeminski.githubactions.actions.actions
 
 import it.krzeminski.githubactions.actions.ActionWithOutputs
-import kotlin.Deprecated
 import kotlin.String
 import kotlin.Suppress
 import kotlin.collections.List
@@ -21,25 +18,25 @@ import kotlin.collections.toTypedArray
  *
  * [Action on GitHub](https://github.com/actions/setup-python)
  */
-@Deprecated(
-    message = "This action has a newer major version: SetupPythonV4",
-    replaceWith = ReplaceWith("SetupPythonV4"),
-)
-public class SetupPythonV3(
+public class SetupPythonV4(
     /**
-     * Version range or exact version of a Python version to use, using SemVer's version range
-     * syntax.
+     * Version range or exact version of Python to use, using SemVer's version range syntax. Reads
+     * from .python-version if unset.
      */
     public val pythonVersion: String? = null,
+    /**
+     * File containing the Python version to use. Example: .python-version
+     */
+    public val pythonVersionFile: String? = null,
     /**
      * Used to specify a package manager for caching in the default directory. Supported values:
      * pip, pipenv, poetry.
      */
-    public val cache: SetupPythonV3.PackageManager? = null,
+    public val cache: SetupPythonV4.PackageManager? = null,
     /**
      * The target architecture (x86, x64) of the Python interpreter.
      */
-    public val architecture: SetupPythonV3.Architecture? = null,
+    public val architecture: SetupPythonV4.Architecture? = null,
     /**
      * Used to pull python distributions from actions/python-versions. Since there's a default, this
      * is typically not supplied by the user.
@@ -59,11 +56,12 @@ public class SetupPythonV3(
      * version that the wrapper doesn't yet know about
      */
     _customVersion: String? = null,
-) : ActionWithOutputs<SetupPythonV3.Outputs>("actions", "setup-python", _customVersion ?: "v3") {
+) : ActionWithOutputs<SetupPythonV4.Outputs>("actions", "setup-python", _customVersion ?: "v4") {
     @Suppress("SpreadOperator")
     public override fun toYamlArguments() = linkedMapOf(
         *listOfNotNull(
             pythonVersion?.let { "python-version" to it },
+            pythonVersionFile?.let { "python-version-file" to it },
             cache?.let { "cache" to it.stringValue },
             architecture?.let { "architecture" to it.stringValue },
             token?.let { "token" to it },
@@ -77,27 +75,27 @@ public class SetupPythonV3(
     public sealed class PackageManager(
         public val stringValue: String,
     ) {
-        public object Pip : SetupPythonV3.PackageManager("pip")
+        public object Pip : SetupPythonV4.PackageManager("pip")
 
-        public object Pipenv : SetupPythonV3.PackageManager("pipenv")
+        public object Pipenv : SetupPythonV4.PackageManager("pipenv")
 
-        public object Poetry : SetupPythonV3.PackageManager("poetry")
+        public object Poetry : SetupPythonV4.PackageManager("poetry")
 
         public class Custom(
             customStringValue: String,
-        ) : SetupPythonV3.PackageManager(customStringValue)
+        ) : SetupPythonV4.PackageManager(customStringValue)
     }
 
     public sealed class Architecture(
         public val stringValue: String,
     ) {
-        public object X64 : SetupPythonV3.Architecture("x64")
+        public object X64 : SetupPythonV4.Architecture("x64")
 
-        public object X86 : SetupPythonV3.Architecture("x86")
+        public object X86 : SetupPythonV4.Architecture("x86")
 
         public class Custom(
             customStringValue: String,
-        ) : SetupPythonV3.Architecture(customStringValue)
+        ) : SetupPythonV4.Architecture(customStringValue)
     }
 
     public class Outputs(
@@ -112,6 +110,11 @@ public class SetupPythonV3(
          * A boolean value to indicate a cache entry was found
          */
         public val cacheHit: String = "steps.$stepId.outputs.cache-hit"
+
+        /**
+         * The absolute path to the Python executable.
+         */
+        public val pythonPath: String = "steps.$stepId.outputs.python-path"
 
         public operator fun `get`(outputName: String) = "steps.$stepId.outputs.$outputName"
     }
