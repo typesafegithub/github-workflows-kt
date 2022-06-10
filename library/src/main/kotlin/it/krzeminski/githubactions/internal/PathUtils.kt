f88e648b953732561658e2350fc0f242ebf82fd4
Piotr Krzemiński <git@krzeminski.it>
2022-06-10 11:39:05 +0200

    [#129] feat(library): allow passing __FILE__ as source
    There's a new feature in Kotlin scripting - `__FILE__` resolves to a
    `File` object pointing to the script that it's being ran. It can be
    leveraged to allow regenerating the script from any place the user
    wishes, e.g. from IntelliJ or from `.github/workflows` directory. The
    logic added here infers git root path and ensures the YAML is correctly
    generated.
    
    Co-authored-by: NikkyAI <git@nikky.moe>

---
+++ library/src/main/kotlin/it/krzeminski/githubactions/internal/PathUtils.kt
@@ -0,0 +1,17 @@
+package it.krzeminski.githubactions.internal
+
+import java.nio.file.Path
+import kotlin.io.path.absolute
+import kotlin.io.path.exists
+import kotlin.io.path.isDirectory
+import kotlin.io.path.relativeTo
+
+internal fun Path.findGitRoot(): Path {
+    return generateSequence(absolute()) { it.parent }
+        .firstOrNull {
+            it.resolve(".git")?.let { it.exists() && it.isDirectory() } ?: false
+        } ?: error("could not find a git root from ${this.absolute()}")
+}
+
+internal fun Path.relativeToAbsolute(base: Path): Path =
+    absolute().relativeTo(base.absolute())
