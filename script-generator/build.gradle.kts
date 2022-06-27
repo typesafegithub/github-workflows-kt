5e44379524c2bef5176c8349ef704ef0a364f028
Piotr Krzemiński <git@krzeminski.it>
2022-06-27 08:14:10 +0200

    chore: divide dependencies from test dependencies

--- script-generator/build.gradle.kts
+++ script-generator/build.gradle.kts
@@ -18,10 +18,10 @@ dependencies {
     implementation(projects.library)
     implementation("com.charleskorn.kaml:kaml:0.46.0")
     implementation("com.squareup:kotlinpoet:1.12.0")
+    implementation(kotlin("reflect"))
 
     testImplementation("io.kotest:kotest-assertions-core:5.3.1")
     testImplementation("io.kotest:kotest-runner-junit5:5.3.1")
-    implementation(kotlin("reflect"))
 }
 
 tasks.withType<Test> {
