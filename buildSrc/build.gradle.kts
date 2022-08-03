f3d175c3dbcc578a37d37c736246c20d517151b1
Adam <897017+aSemy@users.noreply.github.com>
2022-08-03 08:03:24 +0200

    inline versions, use this.freeCompilerArgs to hide IJ warning

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,21 +3,14 @@ plugins {
     kotlin("jvm") version embeddedKotlinVersion
 }
 
-object Versions {
-    const val kotlin = "1.7.10"
-    const val detekt = "1.21.0"
-    const val ktlint = "10.3.0"
-    const val nexusPublishPlugin = "1.1.0"
-}
-
 dependencies {
-    implementation(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:${Versions.kotlin}"))
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+    implementation(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:1.7.10"))
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     implementation("org.jetbrains.kotlin:kotlin-serialization")
 
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${Versions.detekt}")
-    implementation("org.jlleitschuh.gradle:ktlint-gradle:${Versions.ktlint}")
-    implementation("io.github.gradle-nexus:publish-plugin:${Versions.nexusPublishPlugin}")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.21.0")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
+    implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 
     implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.4"))
     implementation(("org.jetbrains.kotlinx:kotlinx-coroutines-core"))
@@ -25,7 +18,7 @@ dependencies {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        freeCompilerArgs += listOf(
+        this.freeCompilerArgs += listOf(
             "-opt-in=kotlin.time.ExperimentalTime",
         )
     }
