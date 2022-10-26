a8612fca3189f1bf65c8868018c624dafc07eb61
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2022-10-26 22:53:23 +0200

    [#407] feat(wrapper-generator): generate typing YAMLs (#518)
    This new `actions` directory will once become the source of truth of
    actions being generated.
    
    Outputs still need to be properly typed, but it's not needed here until
    someone will want to take some typings and introduce them in the action
    itself.

--- wrapper-generator/src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/types/ActionTypesTest.kt
+++ wrapper-generator/src/test/kotlin/it/krzeminski/githubactions/wrappergenerator/types/ActionTypesTest.kt
@@ -2,16 +2,12 @@ package it.krzeminski.githubactions.wrappergenerator.types
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import it.krzeminski.githubactions.wrappergenerator.domain.ActionCoords
-import it.krzeminski.githubactions.wrappergenerator.domain.TypingsSource
-import it.krzeminski.githubactions.wrappergenerator.domain.WrapperRequest
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.BooleanTyping
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.EnumTyping
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.IntegerTyping
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.IntegerWithSpecialValueTyping
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.ListOfTypings
 import it.krzeminski.githubactions.wrappergenerator.domain.typings.StringTyping
-import java.io.File
 
 class ActionTypesTest : FunSpec({
     test("proposed 'type' attribute for 'action.yaml'") {
@@ -41,27 +37,5 @@ class ActionTypesTest : FunSpec({
             separator = ",",
             listItem = ActionType(ActionTypeEnum.Enum, allowedValues = enumType.items),
         )
-    }
-
-    test("wrapper request to YAML") {
-        fun String.noWindowsEnding() = replace("\r", "")
-
-        val request = WrapperRequest(
-            ActionCoords("actions", "github-script", "v6"),
-            TypingsSource.WrapperGenerator(
-                mapOf(
-                    "debug" to BooleanTyping,
-                    "previews" to ListOfTypings(","),
-                    "result-encoding" to EnumTyping("Encoding", listOf("string", "json"))
-                )
-            )
-        )
-        val resources = File("src/test/resources")
-        val actionYml = resources.resolve("types/action.yml")
-        val actionTypesYml = resources.resolve("types/action-types.yml")
-            .readText()
-            .noWindowsEnding()
-
-        request.toYaml(actionYml) shouldBe actionTypesYml
     }
 })
