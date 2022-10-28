dd98bb95b45c2471fa46f73d32881c780128b114
Piotr Krzeminski <git@krzeminski.it>
2022-10-28 20:55:16 +0200

    [#407] docs: mark actions providing typings

--- wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/GenerationEntryPoint.kt
+++ wrapper-generator/src/main/kotlin/it/krzeminski/githubactions/wrappergenerator/GenerationEntryPoint.kt
@@ -2,6 +2,8 @@ package it.krzeminski.githubactions.wrappergenerator
 
 import it.krzeminski.githubactions.dsl.expressions.generateEventPayloads
 import it.krzeminski.githubactions.wrappergenerator.domain.ActionCoords
+import it.krzeminski.githubactions.wrappergenerator.domain.TypingsSource
+import it.krzeminski.githubactions.wrappergenerator.domain.WrapperRequest
 import it.krzeminski.githubactions.wrappergenerator.generation.buildActionClassName
 import it.krzeminski.githubactions.wrappergenerator.generation.generateWrapper
 import it.krzeminski.githubactions.wrappergenerator.generation.toKotlinPackageName
@@ -57,17 +59,24 @@ private fun generateListOfWrappersForDocs(listOfWrappersInDocs: Path) {
         )
 
         wrappersToGenerate
-            .map { it.actionCoords }
-            .groupBy { it.owner }
+            .groupBy { it.actionCoords.owner }
             .forEach { (owner, ownedActions) ->
                 writer.println("* $owner")
                 ownedActions
-                    .groupBy { it.name }
+                    .groupBy { it.actionCoords.name }
                     .forEach { (_, versions) ->
-                        val kotlinClasses = versions.sortedBy { Version(it.version) }.joinToString(", ") { it.toMarkdownLinkToKotlinCode() }
-                        writer.println("    * ${versions.first().toMarkdownLinkGithub()} - $kotlinClasses")
+                        val kotlinClasses = versions
+                            .sortedBy { Version(it.actionCoords.version) }
+                            .joinToString(", ") { it.toMarkdownLinkToKotlinCode() }
+                        writer.println("    * ${versions.first().actionCoords.toMarkdownLinkGithub()} - $kotlinClasses")
                     }
             }
+
+        val uniqueActionsCount = wrappersToGenerate.groupBy { "${it.actionCoords.owner}/${it.actionCoords.name}" }.size
+        val uniqueActionsProvidingTypingsCount = wrappersToGenerate
+            .groupBy { "${it.actionCoords.owner}/${it.actionCoords.name}" }
+            .mapValues { (_, versions) -> versions.maxByOrNull { Version(it.actionCoords.version) } }
+            .count { (_, wrapperRequest) -> wrapperRequest?.typingsSource == TypingsSource.ActionTypes }
 
         writer.println(
             """
@@ -76,15 +85,19 @@ private fun generateListOfWrappersForDocs(listOfWrappersInDocs: Path) {
 
             Number of wrappers available:
 
-            * counting by actions: ${wrappersToGenerate.groupBy { "${it.actionCoords.owner}/${it.actionCoords.name}" }.size}
+            * counting by actions: $uniqueActionsCount
             * counting each version separately: ${wrappersToGenerate.size}
+
+            Actions [providing typings](https://github.com/krzema12/github-actions-typing/) (marked with ✅ on the above list): $uniqueActionsProvidingTypingsCount
             """.trimIndent()
         )
     }
 }
 
-private fun ActionCoords.toMarkdownLinkToKotlinCode() =
-    "$version: [`${buildActionClassName()}`](https://github.com/krzema12/github-workflows-kt/blob/v[[ version ]]/library/src/gen/kotlin/it/krzeminski/githubactions/actions/${owner.toKotlinPackageName()}/${this.buildActionClassName()}.kt)"
+private fun WrapperRequest.toMarkdownLinkToKotlinCode(): String {
+    val typingsMarker = if (typingsSource == TypingsSource.ActionTypes) " ✅" else ""
+    return "${actionCoords.version}$typingsMarker: [`${actionCoords.buildActionClassName()}`](https://github.com/krzema12/github-workflows-kt/blob/v[[ version ]]/library/src/gen/kotlin/it/krzeminski/githubactions/actions/${actionCoords.owner.toKotlinPackageName()}/${this.actionCoords.buildActionClassName()}.kt)"
+}
 
 private fun ActionCoords.toMarkdownLinkGithub() =
     "[$name](https://github.com/$owner/$name)"
