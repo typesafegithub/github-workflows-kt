532f35ff29484cd77d50bcd97d5452c9935f466a
Piotr Krzeminski <git@krzeminski.it>
2023-01-25 20:02:41 +0100

    [#648] fix(wrapper-generator): adjust action versions job after adding support for actions in subdirectories

--- automation/typings/src/main/kotlin/it/krzeminski/githubactions/actionsmetadata/model/ActionCoords.kt
+++ automation/typings/src/main/kotlin/it/krzeminski/githubactions/actionsmetadata/model/ActionCoords.kt
@@ -7,4 +7,10 @@ data class ActionCoords(
     val deprecatedByVersion: String? = null,
 )
 
+/**
+ * A top-level action is an action with its `action.y(a)ml` file in the repository root, as opposed to actions stored
+ * in subdirectories.
+ */
+val ActionCoords.isTopLevel: Boolean get() = "/" !in name
+
 internal val ActionCoords.prettyPrint: String get() = "$owner/$name@$version"
