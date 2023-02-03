ef3269c4f9a01d0e013612500cdc8cd5cb806d24
Piotr Krzeminski <git@krzeminski.it>
2023-02-03 08:35:19 +0100

    feat(actions): update actions/cache@v3

--- library/src/gen/kotlin/it/krzeminski/githubactions/actions/actions/CacheV3.kt
+++ library/src/gen/kotlin/it/krzeminski/githubactions/actions/actions/CacheV3.kt
@@ -45,6 +45,10 @@ public data class CacheV3(
      */
     public val enableCrossOsArchive: Boolean? = null,
     /**
+     * Fail the workflow if cache entry is not found
+     */
+    public val failOnCacheMiss: Boolean? = null,
+    /**
      * Type-unsafe map where you can put any inputs that are not yet supported by the wrapper
      */
     public val _customInputs: Map<String, String> = mapOf(),
@@ -62,6 +66,7 @@ public data class CacheV3(
             restoreKeys?.let { "restore-keys" to it.joinToString("\n") },
             uploadChunkSize?.let { "upload-chunk-size" to it.toString() },
             enableCrossOsArchive?.let { "enableCrossOsArchive" to it.toString() },
+            failOnCacheMiss?.let { "fail-on-cache-miss" to it.toString() },
             *_customInputs.toList().toTypedArray(),
         ).toTypedArray()
     )
