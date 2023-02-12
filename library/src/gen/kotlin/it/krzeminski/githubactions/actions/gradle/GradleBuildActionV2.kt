a2e4019c018a56714b9eb982fb368e140dae27d7
Piotr Krzeminski <git@krzeminski.it>
2023-02-12 22:17:45 +0100

    fix

--- library/src/gen/kotlin/it/krzeminski/githubactions/actions/gradle/GradleBuildActionV2.kt
+++ library/src/gen/kotlin/it/krzeminski/githubactions/actions/gradle/GradleBuildActionV2.kt
@@ -37,7 +37,7 @@ public data class GradleBuildActionV2(
     public val cacheReadOnly: Boolean? = null,
     /**
      * When 'true', entries will not be restored from the cache but will be saved at the end of the
-     * Job.
+     * Job. 
      * Setting this to 'true' implies cache-read-only will be 'false'.
      */
     public val cacheWriteOnly: Boolean? = null,
