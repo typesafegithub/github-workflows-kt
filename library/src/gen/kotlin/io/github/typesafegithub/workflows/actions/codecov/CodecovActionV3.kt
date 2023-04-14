8b2b2faab91a13e002278e346f050f1e4981aa1b
Piotr Krzeminski <git@krzeminski.it>
2023-04-14 22:28:10 +0200

    feat(actions): update codecov/codecov-action@v3

--- library/src/gen/kotlin/io/github/typesafegithub/workflows/actions/codecov/CodecovActionV3.kt
+++ library/src/gen/kotlin/io/github/typesafegithub/workflows/actions/codecov/CodecovActionV3.kt
@@ -34,6 +34,10 @@ public data class CodecovActionV3 private constructor(
      */
     public val token: String? = null,
     /**
+     * Path to coverage file to upload
+     */
+    public val `file`: String? = null,
+    /**
      * Comma-separated list of files to upload
      */
     public val files: List<String>? = null,
@@ -45,6 +49,10 @@ public data class CodecovActionV3 private constructor(
      * Flag upload to group coverage metrics (e.g. unittests | integration | ui,chrome)
      */
     public val flags: List<String>? = null,
+    /**
+     * Specify the path of a full Codecov report to re-upload
+     */
+    public val fullReport: String? = null,
     /**
      * The commit SHA of the parent for which you are uploading coverage. If not present, the parent
      * will be determined using the API of your repository provider.  When using the repository
@@ -64,11 +72,8 @@ public data class CodecovActionV3 private constructor(
      */
     public val failCiIfError: Boolean? = null,
     /**
-     * Path to coverage file to upload
-     */
-    public val `file`: String? = null,
-    /**
-     * Comma-separated list, see the README for options and their usage
+     * Comma-separated list, see the README for options and their usage. Options include `network`,
+     * `fixes`, `search`.
      */
     public val functionalities: List<String>? = null,
     /**
@@ -79,6 +84,10 @@ public data class CodecovActionV3 private constructor(
      * Extra arguments to pass to gcov
      */
     public val gcovArgs: List<String>? = null,
+    /**
+     * gcov executable to run. Defaults to gcov
+     */
+    public val gcovExecutable: String? = null,
     /**
      * Paths to ignore during gcov gathering
      */
@@ -95,6 +104,20 @@ public data class CodecovActionV3 private constructor(
      * User defined upload name. Visible in Codecov UI
      */
     public val name: String? = null,
+    /**
+     * Specify a filter on the files listed in the network section of the Codecov report. Useful for
+     * upload-specific path fixing
+     */
+    public val networkFilter: String? = null,
+    /**
+     * Specify a prefix on files listed in the network section of the Codecov report. Useful to help
+     * resolve path fixing
+     */
+    public val networkPrefix: String? = null,
+    /**
+     * Override the assumed OS. Options are alpine | linux | macos | windows.
+     */
+    public val os: CodecovActionV3.OperatingSystem? = null,
     /**
      * Specify the branch name
      */
@@ -116,10 +139,6 @@ public data class CodecovActionV3 private constructor(
      */
     public val overrideTag: String? = null,
     /**
-     * Override the assumed OS. Options are alpine | linux | macos | windows.
-     */
-    public val os: CodecovActionV3.OperatingSystem? = null,
-    /**
      * Used when not in git/hg project to identify project root directory
      */
     public val rootDir: String? = null,
@@ -127,6 +146,18 @@ public data class CodecovActionV3 private constructor(
      * Specify the slug manually (Enterprise use)
      */
     public val slug: String? = null,
+    /**
+     * Run with swift coverage support
+     */
+    public val swift: Boolean? = null,
+    /**
+     * Specify the swift project to speed up coverage conversion
+     */
+    public val swiftProject: String? = null,
+    /**
+     * The upstream http proxy server to connect through
+     */
+    public val upstreamProxy: String? = null,
     /**
      * Change the upload host (Enterprise use)
      */
@@ -140,10 +171,6 @@ public data class CodecovActionV3 private constructor(
      */
     public val version: String? = null,
     /**
-     * Directory in which to execute codecov.sh
-     */
-    public val workingDirectory: String? = null,
-    /**
      * Run with xcode support
      */
     public val xcode: Boolean? = null,
@@ -152,6 +179,10 @@ public data class CodecovActionV3 private constructor(
      * .xcresult
      */
     public val xcodeArchivePath: String? = null,
+    /**
+     * Add additional uploader args that may be missing in the Action
+     */
+    public val xtraArgs: String? = null,
     /**
      * Type-unsafe map where you can put any inputs that are not yet supported by the wrapper
      */
@@ -165,80 +196,97 @@ public data class CodecovActionV3 private constructor(
     public constructor(
         vararg pleaseUseNamedArguments: Unit,
         token: String? = null,
+        `file`: String? = null,
         files: List<String>? = null,
         directory: String? = null,
         flags: List<String>? = null,
+        fullReport: String? = null,
         commitParent: String? = null,
         dryRun: Boolean? = null,
         envVars: List<String>? = null,
         failCiIfError: Boolean? = null,
-        `file`: String? = null,
         functionalities: List<String>? = null,
         gcov: Boolean? = null,
         gcovArgs: List<String>? = null,
+        gcovExecutable: String? = null,
         gcovIgnore: List<String>? = null,
         gcovInclude: List<String>? = null,
         moveCoverageToTrash: Boolean? = null,
         name: String? = null,
+        networkFilter: String? = null,
+        networkPrefix: String? = null,
+        os: CodecovActionV3.OperatingSystem? = null,
         overrideBranch: String? = null,
         overrideBuild: Int? = null,
         overrideCommit: String? = null,
         overridePr: Int? = null,
         overrideTag: String? = null,
-        os: CodecovActionV3.OperatingSystem? = null,
         rootDir: String? = null,
         slug: String? = null,
+        swift: Boolean? = null,
+        swiftProject: String? = null,
+        upstreamProxy: String? = null,
         url: String? = null,
         verbose: Boolean? = null,
         version: String? = null,
-        workingDirectory: String? = null,
         xcode: Boolean? = null,
         xcodeArchivePath: String? = null,
+        xtraArgs: String? = null,
         _customInputs: Map<String, String> = mapOf(),
         _customVersion: String? = null,
-    ) : this(token=token, files=files, directory=directory, flags=flags, commitParent=commitParent,
-            dryRun=dryRun, envVars=envVars, failCiIfError=failCiIfError, `file`=`file`,
-            functionalities=functionalities, gcov=gcov, gcovArgs=gcovArgs, gcovIgnore=gcovIgnore,
+    ) : this(token=token, `file`=`file`, files=files, directory=directory, flags=flags,
+            fullReport=fullReport, commitParent=commitParent, dryRun=dryRun, envVars=envVars,
+            failCiIfError=failCiIfError, functionalities=functionalities, gcov=gcov,
+            gcovArgs=gcovArgs, gcovExecutable=gcovExecutable, gcovIgnore=gcovIgnore,
             gcovInclude=gcovInclude, moveCoverageToTrash=moveCoverageToTrash, name=name,
+            networkFilter=networkFilter, networkPrefix=networkPrefix, os=os,
             overrideBranch=overrideBranch, overrideBuild=overrideBuild,
-            overrideCommit=overrideCommit, overridePr=overridePr, overrideTag=overrideTag, os=os,
-            rootDir=rootDir, slug=slug, url=url, verbose=verbose, version=version,
-            workingDirectory=workingDirectory, xcode=xcode, xcodeArchivePath=xcodeArchivePath,
-            _customInputs=_customInputs, _customVersion=_customVersion)
+            overrideCommit=overrideCommit, overridePr=overridePr, overrideTag=overrideTag,
+            rootDir=rootDir, slug=slug, swift=swift, swiftProject=swiftProject,
+            upstreamProxy=upstreamProxy, url=url, verbose=verbose, version=version, xcode=xcode,
+            xcodeArchivePath=xcodeArchivePath, xtraArgs=xtraArgs, _customInputs=_customInputs,
+            _customVersion=_customVersion)
 
     @Suppress("SpreadOperator")
     public override fun toYamlArguments(): LinkedHashMap<String, String> = linkedMapOf(
         *listOfNotNull(
             token?.let { "token" to it },
+            `file`?.let { "file" to it },
             files?.let { "files" to it.joinToString(",") },
             directory?.let { "directory" to it },
             flags?.let { "flags" to it.joinToString(",") },
+            fullReport?.let { "full_report" to it },
             commitParent?.let { "commit_parent" to it },
             dryRun?.let { "dry_run" to it.toString() },
             envVars?.let { "env_vars" to it.joinToString(",") },
             failCiIfError?.let { "fail_ci_if_error" to it.toString() },
-            `file`?.let { "file" to it },
             functionalities?.let { "functionalities" to it.joinToString(",") },
             gcov?.let { "gcov" to it.toString() },
             gcovArgs?.let { "gcov_args" to it.joinToString(" ") },
+            gcovExecutable?.let { "gcov_executable" to it },
             gcovIgnore?.let { "gcov_ignore" to it.joinToString("\n") },
             gcovInclude?.let { "gcov_include" to it.joinToString("\n") },
             moveCoverageToTrash?.let { "move_coverage_to_trash" to it.toString() },
             name?.let { "name" to it },
+            networkFilter?.let { "network_filter" to it },
+            networkPrefix?.let { "network_prefix" to it },
+            os?.let { "os" to it.stringValue },
             overrideBranch?.let { "override_branch" to it },
             overrideBuild?.let { "override_build" to it.toString() },
             overrideCommit?.let { "override_commit" to it },
             overridePr?.let { "override_pr" to it.toString() },
             overrideTag?.let { "override_tag" to it },
-            os?.let { "os" to it.stringValue },
             rootDir?.let { "root_dir" to it },
             slug?.let { "slug" to it },
+            swift?.let { "swift" to it.toString() },
+            swiftProject?.let { "swift_project" to it },
+            upstreamProxy?.let { "upstream_proxy" to it },
             url?.let { "url" to it },
             verbose?.let { "verbose" to it.toString() },
             version?.let { "version" to it },
-            workingDirectory?.let { "working-directory" to it },
             xcode?.let { "xcode" to it.toString() },
             xcodeArchivePath?.let { "xcode_archive_path" to it },
+            xtraArgs?.let { "xtra_args" to it },
             *_customInputs.toList().toTypedArray(),
         ).toTypedArray()
     )
