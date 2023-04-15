97b2611f1f36d68666222eaffb09f8798fe34ce1
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2023-04-15 23:03:19 +0200

    refactor(script-generator)!: split logic and CLI into two modules (#760)
    After this change, `yamlToKotlinScript` function from
    `script-generator/logic/src/main/kotlin/io/github/typesafegithub/workflows/scriptgenerator/YamlToKotlinScript.kt`
    becomes a simple API reusable by any user-facing interface like CLI or REST API.

--- script-generator/logic/src/test/kotlin/test/GenerateKotlinScripts.kt
+++ script-generator/logic/src/test/kotlin/test/GenerateKotlinScripts.kt
@@ -1,9 +1,7 @@
 package test
 
-import io.github.typesafegithub.workflows.scriptgenerator.decodeYamlWorkflow
 import io.github.typesafegithub.workflows.scriptgenerator.rootProject
-import io.github.typesafegithub.workflows.scriptgenerator.toFileSpec
-import io.github.typesafegithub.workflows.scriptmodel.YamlWorkflow
+import io.github.typesafegithub.workflows.scriptgenerator.yamlToKotlinScript
 import io.github.typesafegithub.workflows.wrappergenerator.generation.toPascalCase
 import io.kotest.assertions.fail
 import io.kotest.core.spec.style.FunSpec
@@ -12,18 +10,15 @@ import java.io.FileFilter
 
 class GenerateKotlinScripts : FunSpec({
 
-    val testInputs = rootProject.resolve("script-generator/yaml-input")
+    val testInputs = rootProject.resolve("script-generator/logic/yaml-input")
         .listFiles(FileFilter { it.extension == "yaml" })!!
         .map { it.name }
 
     testInputs.forEach { name ->
         test("Generating $name") {
             val input = TestInput(name)
-            val workflow: YamlWorkflow = decodeYamlWorkflow(input.yamlFile.readText())
 
-            val newContent = workflow.toFileSpec(workflow.name)
-                .toBuilder(packageName = "generated").build()
-                .toString()
+            val newContent = yamlToKotlinScript(yaml = input.yamlFile.readText())
                 .removeWindowsEndings()
 
             input.actualFile.writeText(
@@ -44,12 +39,12 @@ class GenerateKotlinScripts : FunSpec({
 
 data class TestInput(val name: String) {
     val filename = name.removeSuffix(".yml")
-    val yamlFile = rootProject.resolve("script-generator/yaml-input/$filename.yml")
+    val yamlFile = rootProject.resolve("script-generator/logic/yaml-input/$filename.yml")
         .also { require(it.canRead()) { "Invalid file ${it.canonicalPath}" } }
 
-    private fun file(path: String) = rootProject.resolve("script-generator/src/test/kotlin/generated/$path")
+    private fun file(path: String) = rootProject.resolve("script-generator/logic/src/test/kotlin/generated/$path")
     val expectedFile = file("${filename.toPascalCase()}.kt")
-    val actualDir = rootProject.resolve("script-generator/src/test/kotlin/actual")
+    val actualDir = rootProject.resolve("script-generator/logic/src/test/kotlin/actual")
         .also { it.mkdirs() }
     val actualFile = actualDir.resolve("${filename.toPascalCase()}.kt")
     val expected = if (expectedFile.canRead()) {
