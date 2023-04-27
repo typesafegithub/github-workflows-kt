5dc949777f907facc01ec50e29c51a1f5d126f01
Bens Keil <benkeil.me@googlemail.com>
2023-04-27 14:09:17 +0200

    fix: revert indentation

--- library/src/test/kotlin/io/github/typesafegithub/workflows/dsl/WorkflowBuilderTest.kt
+++ library/src/test/kotlin/io/github/typesafegithub/workflows/dsl/WorkflowBuilderTest.kt
@@ -7,121 +7,119 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import java.nio.file.Paths
 
-class WorkflowBuilderTest : FunSpec(
-    {
-        context("validation errors") {
-            test("no jobs defined") {
-                val exception = shouldThrow<IllegalArgumentException> {
-                    workflow(
-                        name = "Some workflow",
-                        on = listOf(Push()),
-                        sourceFile = Paths.get(".github/workflows/some_workflow.main.kts"),
-                    ) {
-                        // No jobs.
-                    }
+class WorkflowBuilderTest : FunSpec({
+    context("validation errors") {
+        test("no jobs defined") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                workflow(
+                    name = "Some workflow",
+                    on = listOf(Push()),
+                    sourceFile = Paths.get(".github/workflows/some_workflow.main.kts"),
+                ) {
+                    // No jobs.
                 }
-                exception.message shouldBe "There are no jobs defined!"
             }
-
-            test("no steps defined") {
-                val exception = shouldThrow<IllegalArgumentException> {
-                    workflow(
-                        name = "Some workflow",
-                        on = listOf(Push()),
-                        sourceFile = Paths.get(".github/workflows/some_workflow.main.kts"),
-                    ) {
-                        job(
-                            id = "Some-job",
-                            runsOn = UbuntuLatest,
-                        ) {
-                            // No steps.
-                        }
-                    }
-                }
-                exception.message shouldBe "There are no steps defined!"
-            }
-
-            test("no triggers defined") {
-                val exception = shouldThrow<IllegalArgumentException> {
-                    workflow(
-                        name = "Some workflow",
-                        on = emptyList(),
-                        sourceFile = Paths.get(".github/workflows/some_workflow.main.kts"),
-                    ) {
-                        job(
-                            id = "Some job",
-                            runsOn = UbuntuLatest,
-                        ) {
-                            run(
-                                name = "Some command",
-                                command = "echo 'hello!'",
-                            )
-                        }
-                    }
-                }
-                exception.message shouldBe "There are no triggers defined!"
-            }
-
-            test("duplicated job names") {
-                val exception = shouldThrow<IllegalArgumentException> {
-                    workflow(
-                        name = "Some workflow",
-                        on = listOf(Push()),
-                        sourceFile = Paths.get(".github/workflows/some_workflow.main.kts"),
-                    ) {
-                        job(
-                            id = "Some-job-1",
-                            runsOn = UbuntuLatest,
-                        ) {
-                            run(
-                                name = "Some command",
-                                command = "echo 'hello!'",
-                            )
-                        }
-
-                        job(
-                            id = "Some-job-1",
-                            runsOn = UbuntuLatest,
-                        ) {
-                            run(
-                                name = "Some command",
-                                command = "echo 'hello!'",
-                            )
-                        }
-
-                        job(
-                            id = "Some-job-2",
-                            runsOn = UbuntuLatest,
-                        ) {
-                            run(
-                                name = "Some command",
-                                command = "echo 'hello!'",
-                            )
-                        }
-
-                        job(
-                            id = "Some-job-3",
-                            runsOn = UbuntuLatest,
-                        ) {
-                            run(
-                                name = "Some command",
-                                command = "echo 'hello!'",
-                            )
-                        }
-
-                        job(
-                            id = "Some-job-3",
-                            runsOn = UbuntuLatest,
-                        ) {
-                            run(
-                                name = "Some command",
-                                command = "echo 'hello!'",
-                            )
-                        }
-                    }
-                }
-                exception.message shouldBe "Duplicated job names: [Some-job-1, Some-job-3]"
-            }
+            exception.message shouldBe "There are no jobs defined!"
         }
-    },
-)
+
+        test("no steps defined") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                workflow(
+                    name = "Some workflow",
+                    on = listOf(Push()),
+                    sourceFile = Paths.get(".github/workflows/some_workflow.main.kts"),
+                ) {
+                    job(
+                        id = "Some-job",
+                        runsOn = UbuntuLatest,
+                    ) {
+                        // No steps.
+                    }
+                }
+            }
+            exception.message shouldBe "There are no steps defined!"
+        }
+
+        test("no triggers defined") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                workflow(
+                    name = "Some workflow",
+                    on = emptyList(),
+                    sourceFile = Paths.get(".github/workflows/some_workflow.main.kts"),
+                ) {
+                    job(
+                        id = "Some job",
+                        runsOn = UbuntuLatest,
+                    ) {
+                        run(
+                            name = "Some command",
+                            command = "echo 'hello!'",
+                        )
+                    }
+                }
+            }
+            exception.message shouldBe "There are no triggers defined!"
+        }
+
+        test("duplicated job names") {
+            val exception = shouldThrow<IllegalArgumentException> {
+                workflow(
+                    name = "Some workflow",
+                    on = listOf(Push()),
+                    sourceFile = Paths.get(".github/workflows/some_workflow.main.kts"),
+                ) {
+                    job(
+                        id = "Some-job-1",
+                        runsOn = UbuntuLatest,
+                    ) {
+                        run(
+                            name = "Some command",
+                            command = "echo 'hello!'",
+                        )
+                    }
+
+                    job(
+                        id = "Some-job-1",
+                        runsOn = UbuntuLatest,
+                    ) {
+                        run(
+                            name = "Some command",
+                            command = "echo 'hello!'",
+                        )
+                    }
+
+                    job(
+                        id = "Some-job-2",
+                        runsOn = UbuntuLatest,
+                    ) {
+                        run(
+                            name = "Some command",
+                            command = "echo 'hello!'",
+                        )
+                    }
+
+                    job(
+                        id = "Some-job-3",
+                        runsOn = UbuntuLatest,
+                    ) {
+                        run(
+                            name = "Some command",
+                            command = "echo 'hello!'",
+                        )
+                    }
+
+                    job(
+                        id = "Some-job-3",
+                        runsOn = UbuntuLatest,
+                    ) {
+                        run(
+                            name = "Some command",
+                            command = "echo 'hello!'",
+                        )
+                    }
+                }
+            }
+            exception.message shouldBe "Duplicated job names: [Some-job-1, Some-job-3]"
+        }
+    }
+})
