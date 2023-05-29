8d765f286aeda300776d66ff30e1867c897a0f84
Piotr Krzeminski <git@krzeminski.it>
2023-05-29 22:50:45 +0200

    chore(ci): add workflow for deploying docs

---
+++ .github/workflows/release-docs.main.kts
@@ -0,0 +1,26 @@
+#!/usr/bin/env kotlin
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:0.44.0")
+@file:Import("_shared.main.kts")
+
+import io.github.typesafegithub.workflows.actions.actions.CheckoutV3
+import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
+import io.github.typesafegithub.workflows.domain.triggers.WorkflowDispatch
+import io.github.typesafegithub.workflows.dsl.workflow
+import io.github.typesafegithub.workflows.yaml.writeToFile
+
+workflow(
+    name = "Release docs",
+    on = listOf(WorkflowDispatch()),
+    sourceFile = __FILE__.toPath(),
+) {
+    job(
+        id = "release-docs",
+        runsOn = UbuntuLatest,
+    ) {
+        uses(CheckoutV3())
+        setupJava()
+        setupPython()
+
+        deployDocs()
+    }
+}.writeToFile()
