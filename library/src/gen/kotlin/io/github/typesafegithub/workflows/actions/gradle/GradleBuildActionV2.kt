69ed2da0f8a19bd71af88372737c5f21d34177ac
Piotr Krzeminski <git@krzeminski.it>
2023-06-05 08:34:33 +0200

    Revert whitespace change

--- library/src/gen/kotlin/io/github/typesafegithub/workflows/actions/gradle/GradleBuildActionV2.kt
+++ library/src/gen/kotlin/io/github/typesafegithub/workflows/actions/gradle/GradleBuildActionV2.kt
@@ -44,7 +44,7 @@ public data class GradleBuildActionV2 private constructor(
     public val cacheReadOnly: Boolean? = null,
     /**
      * When 'true', entries will not be restored from the cache but will be saved at the end of the
-     * Job.
+     * Job. 
      * Setting this to 'true' implies cache-read-only will be 'false'.
      */
     public val cacheWriteOnly: Boolean? = null,
