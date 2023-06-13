30f360f0cb89fd3f2cdba36a567a1e8e08fd0abe
Piotr Krzeminski <git@krzeminski.it>
2023-06-13 12:14:45 +0200

    docs: recommend writeToFile() approach instead of toYaml()

--- library/src/test/kotlin/io/github/typesafegithub/workflows/docsnippets/GettingStartedSnippets.kt
+++ library/src/test/kotlin/io/github/typesafegithub/workflows/docsnippets/GettingStartedSnippets.kt
@@ -7,7 +7,7 @@ import io.github.typesafegithub.workflows.actions.actions.CheckoutV3
 import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
 import io.github.typesafegithub.workflows.domain.triggers.Push
 import io.github.typesafegithub.workflows.dsl.workflow
-import io.github.typesafegithub.workflows.yaml.toYaml
+import io.github.typesafegithub.workflows.yaml.writeToFile
 // --8<-- [end:getting-started-2]
 import java.io.File
 /* ktlint-enable import-ordering */
@@ -26,7 +26,7 @@ class GettingStartedSnippets : FunSpec({
         val __FILE__ = File("")
         // --8<-- [start:getting-started-3]
 
-        val workflow = workflow(
+        workflow(
             name = "Test workflow",
             on = listOf(Push()),
             sourceFile = __FILE__.toPath(),
@@ -35,9 +35,7 @@ class GettingStartedSnippets : FunSpec({
                 uses(name = "Check out", action = CheckoutV3())
                 run(name = "Print greeting", command = "echo 'Hello world!'")
             }
-        }
-
-        println(workflow.toYaml())
+        }.writeToFile()
         // --8<-- [end:getting-started-3]
     }
 })
