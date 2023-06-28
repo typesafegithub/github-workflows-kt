a37e7d9d50318e6e5e69f7b61c7a684e935926f9
Szczepan Wiśniowski <szczepan7700@gmail.com>
2023-06-28 11:10:05 +0200

    feat(library): Add type-safe port and volume mappings for containers
    Also add tests for Container.healthCheck and escape double quotes

--- library/src/main/kotlin/io/github/typesafegithub/workflows/domain/Container.kt
+++ library/src/main/kotlin/io/github/typesafegithub/workflows/domain/Container.kt
@@ -12,8 +12,8 @@ public data class Container(
     val image: String,
     val credentials: Credentials? = null,
     val env: LinkedHashMap<String, String> = linkedMapOf(),
-    val ports: List<String> = emptyList(),
-    val volumes: List<String> = emptyList(),
+    val ports: List<PortMapping> = emptyList(),
+    val volumes: List<VolumeMapping> = emptyList(),
     val options: List<String> = emptyList(),
     override val _customArguments: Map<String, @Contextual Any?> = emptyMap(),
 ) : HasCustomArguments {
@@ -28,8 +28,10 @@ public data class Container(
             require(timeoutSeconds > 0) { "timeoutSeconds must be greater than 0" }
             require(retries > 0) { "retries must be greater than 0" }
 
+            val escapedCommand = command.replace("\"", "\"'\"'\"")
+
             return listOf(
-                "--health-cmd \"$command\"",
+                "--health-cmd \"$escapedCommand\"",
                 "--health-interval ${intervalSeconds}s",
                 "--health-timeout ${timeoutSeconds}s",
                 "--health-retries $retries",
@@ -42,3 +44,51 @@ public data class Credentials(
     val username: String,
     val password: String,
 )
+
+/**
+ * @see io.github.typesafegithub.workflows.dsl.port
+ * @see io.github.typesafegithub.workflows.dsl.tcp
+ * @see io.github.typesafegithub.workflows.dsl.udp
+ */
+public data class PortMapping(
+    val host: Int,
+    val container: Int? = null,
+    val protocol: Protocol = Protocol.All,
+) {
+    init {
+        require(host in portRange) { "host port must be in range $portRange" }
+        container?.let { value ->
+            require(value in portRange) { "container port must be in range $portRange" }
+        }
+    }
+
+    private companion object {
+        private val portRange = 1..UShort.MAX_VALUE.toInt()
+    }
+
+    public sealed interface Protocol {
+        public data class Custom(val value: String) : Protocol
+        public object All : Protocol
+        public object TCP : Protocol
+        public object UDP : Protocol
+    }
+}
+
+/**
+ * @see io.github.typesafegithub.workflows.dsl.volume
+ */
+public data class VolumeMapping(
+    val source: String? = null,
+    val target: String,
+    val isReadOnly: Boolean = false,
+) {
+    init {
+        source?.let {
+            require(it.isNotBlank()) { "source must not be blank" }
+            require(':' !in it) { "source must not contain ':'" }
+        }
+
+        require(target.isNotBlank()) { "target must not be blank" }
+        require(':' !in target) { "target must not contain ':'" }
+    }
+}
--- library/src/test/kotlin/io/github/typesafegithub/workflows/yaml/ContainerToYamlTest.kt
+++ library/src/test/kotlin/io/github/typesafegithub/workflows/yaml/ContainerToYamlTest.kt
@@ -2,6 +2,11 @@ package io.github.typesafegithub.workflows.yaml
 
 import io.github.typesafegithub.workflows.domain.Container
 import io.github.typesafegithub.workflows.domain.Credentials
+import io.github.typesafegithub.workflows.domain.PortMapping.Protocol
+import io.github.typesafegithub.workflows.dsl.port
+import io.github.typesafegithub.workflows.dsl.tcp
+import io.github.typesafegithub.workflows.dsl.udp
+import io.github.typesafegithub.workflows.dsl.volume
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
@@ -19,8 +24,14 @@ class ContainerToYamlTest : DescribeSpec({
     it("renders with all arguments") {
         val container = Container(
             image = "test-image",
-            ports = listOf("test-port1", "test-port2"),
-            volumes = listOf("test-volume1", "test-volume2"),
+            ports = listOf(
+                port(8080 to 80),
+                port(22 to 22),
+            ),
+            volumes = listOf(
+                volume("/host/path" to "/container/path"),
+                volume("/host/path2" to "/container/path2"),
+            ),
             env = linkedMapOf("test-env-key" to "test-env-value"),
             options = listOf("test-option1", "test-option2"),
             credentials = Credentials(
@@ -37,8 +48,8 @@ class ContainerToYamlTest : DescribeSpec({
 
         container.toYaml() shouldBe mapOf(
             "image" to "test-image",
-            "ports" to listOf("test-port1", "test-port2"),
-            "volumes" to listOf("test-volume1", "test-volume2"),
+            "ports" to listOf("8080:80", "22:22"),
+            "volumes" to listOf("/host/path:/container/path", "/host/path2:/container/path2"),
             "env" to mapOf("test-env-key" to "test-env-value"),
             "options" to "test-option1 test-option2",
             "credentials" to mapOf(
@@ -50,5 +61,38 @@ class ContainerToYamlTest : DescribeSpec({
             "null-value" to null,
             "empty-string" to "",
         )
+    }
+
+    it("renders volumes") {
+        volume("/container/path")
+            .toYaml() shouldBe "/container/path"
+
+        volume("/container/path", isReadOnly = true)
+            .toYaml() shouldBe "/container/path:ro"
+
+        volume("/host/path" to "/container/path")
+            .toYaml() shouldBe "/host/path:/container/path"
+
+        volume("/host/path" to "/container/path", isReadOnly = true)
+            .toYaml() shouldBe "/host/path:/container/path:ro"
+    }
+
+    it("renders ports") {
+        port(22).toYaml() shouldBe "22"
+        port(22 to 25).toYaml() shouldBe "22:25"
+        port(22, Protocol.Custom("")).toYaml() shouldBe "22"
+        port(22 to 25, Protocol.Custom("")).toYaml() shouldBe "22:25"
+
+        port(22, Protocol.TCP).toYaml() shouldBe "22/tcp"
+        port(22 to 25, Protocol.TCP).toYaml() shouldBe "22:25/tcp"
+        port(22, Protocol.UDP).toYaml() shouldBe "22/udp"
+        port(22 to 25, Protocol.UDP).toYaml() shouldBe "22:25/udp"
+        port(22, Protocol.Custom("test")).toYaml() shouldBe "22/test"
+        port(22 to 25, Protocol.Custom("test")).toYaml() shouldBe "22:25/test"
+
+        tcp(22).toYaml() shouldBe "22/tcp"
+        tcp(22 to 25).toYaml() shouldBe "22:25/tcp"
+        udp(22).toYaml() shouldBe "22/udp"
+        udp(22 to 25).toYaml() shouldBe "22:25/udp"
     }
 })
