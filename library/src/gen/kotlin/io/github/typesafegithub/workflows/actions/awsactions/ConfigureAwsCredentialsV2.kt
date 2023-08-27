133de0a9ff5a616b708c871941f5157b7fea35c3
Piotr Krzeminski <git@krzeminski.it>
2023-08-27 20:22:50 +0200

    [#936] feat(actions): add aws-actions/configure-aws-credentials@v3

--- library/src/gen/kotlin/io/github/typesafegithub/workflows/actions/awsactions/ConfigureAwsCredentialsV2.kt
+++ library/src/gen/kotlin/io/github/typesafegithub/workflows/actions/awsactions/ConfigureAwsCredentialsV2.kt
@@ -4,6 +4,7 @@
 @file:Suppress(
     "DataClassPrivateConstructor",
     "UNUSED_PARAMETER",
+    "DEPRECATION",
 )
 
 package io.github.typesafegithub.workflows.actions.awsactions
@@ -12,6 +13,7 @@ import io.github.typesafegithub.workflows.domain.actions.Action
 import io.github.typesafegithub.workflows.domain.actions.RegularAction
 import java.util.LinkedHashMap
 import kotlin.Boolean
+import kotlin.Deprecated
 import kotlin.Int
 import kotlin.String
 import kotlin.Suppress
@@ -28,6 +30,10 @@ import kotlin.collections.toTypedArray
  *
  * [Action on GitHub](https://github.com/aws-actions/configure-aws-credentials)
  */
+@Deprecated(
+    message = "This action has a newer major version: ConfigureAwsCredentialsV3",
+    replaceWith = ReplaceWith("ConfigureAwsCredentialsV3"),
+)
 public data class ConfigureAwsCredentialsV2 private constructor(
     /**
      * The audience to use for the OIDC provider
