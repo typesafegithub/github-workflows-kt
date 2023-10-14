2012886a9c698af54c80d81fa42041c7ea55a728
Piotr Krzeminski <git@krzeminski.it>
2023-10-14 22:15:47 +0200

    [#941] chore: configure action-binding-generator's publishing

---
+++ buildSrc/src/main/kotlin/buildsrc/convention/action-binding-generator-publishing.gradle.kts
@@ -0,0 +1,76 @@
+package buildsrc.convention
+
+import buildsrc.tasks.AwaitMavenCentralDeployTask
+
+plugins {
+    `maven-publish`
+    signing
+}
+
+val githubUser = "typesafegithub"
+val repositoryName = "github-workflows-kt"
+val mavenLibraryName = "action-binding-generator"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = mavenLibraryName
+            from(components["java"])
+
+            pom {
+                name.set(mavenLibraryName)
+                description.set("A companion library for github-workflows-kt for generating Kotlin bindings for any GitHub action.")
+                url.set("https://github.com/$githubUser/$repositoryName")
+
+                licenses {
+                    license {
+                        name.set("Apache License, version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/$githubUser/$repositoryName.git/")
+                    developerConnection.set("scm:git:ssh://github.com:$githubUser/$repositoryName.git")
+                    url.set("https://github.com/$githubUser/$repositoryName.git")
+                }
+
+                developers {
+                    developer {
+                        id.set(githubUser)
+                        name.set("Piotr Krzemiński")
+                        email.set("git@krzeminski.it")
+                    }
+                }
+            }
+        }
+    }
+
+    val ossrhUsername: String? by project
+    val ossrhPassword: String? by project
+
+    repositories {
+        maven(url = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
+            credentials {
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+signing {
+    if (!project.version.toString().endsWith("-SNAPSHOT") && !project.findProperty("suppressSigning")?.toString().toBoolean()) {
+        sign(publishing.publications["mavenJava"])
+    }
+
+    val signingKey = System.getenv("SIGNING_KEY")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+}
+
+val waitUntilLibraryPresentInMavenCentral by tasks.registering(AwaitMavenCentralDeployTask::class) {
+    this.groupId.set(project.group.toString())
+    this.artifactId.set(mavenLibraryName)
+    this.version.set(project.version.toString())
+}
