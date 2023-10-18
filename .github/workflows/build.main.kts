9be1aaa0ac719c46dc8ef95172540563d77f40bf
Piotr Krzeminski <git@krzeminski.it>
2023-10-18 20:47:07 +0200

    Provide signing keys

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -43,6 +43,8 @@ workflow(
         runsOn = UbuntuLatest,
         needs = buildJobs,
         env = linkedMapOf(
+            "SIGNING_KEY" to expr("secrets.SIGNING_KEY"),
+            "SIGNING_PASSWORD" to expr("secrets.SIGNING_PASSWORD"),
             "ORG_GRADLE_PROJECT_sonatypeUsername" to expr("secrets.ORG_GRADLE_PROJECT_SONATYPEUSERNAME"),
             "ORG_GRADLE_PROJECT_sonatypePassword" to expr("secrets.ORG_GRADLE_PROJECT_SONATYPEPASSWORD"),
         ),
