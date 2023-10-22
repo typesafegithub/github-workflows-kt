2be4e664ac412b734cf624bfe0051dae84c9451e
Piotr Krzeminski <git@krzeminski.it>
2023-10-22 12:17:06 +0200

    [#941] ci: use client-side binding generation for actions-versions workflow

--- .github/workflows/actions-versions.main.kts
+++ .github/workflows/actions-versions.main.kts
@@ -1,9 +1,11 @@
 #!/usr/bin/env kotlin
-@file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.3.1")
-@file:Import("_shared.main.kts")
+@file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.3.2-20231022.100333-12")
+//@file:Import("_shared.main.kts")
 @file:Import("actions/peter-evans/create-issue-from-file.kt")
 
 import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
+import io.github.typesafegithub.workflows.actions.actions.SetupJavaV3
 import io.github.typesafegithub.workflows.actions.gradle.GradleBuildActionV2
 import io.github.typesafegithub.workflows.domain.RunnerType.UbuntuLatest
 import io.github.typesafegithub.workflows.domain.triggers.Cron
@@ -22,15 +24,27 @@ workflow(
         WorkflowDispatch(),
     ),
     sourceFile = __FILE__.toPath(),
-    yamlConsistencyJobCondition = disableScheduledJobInForks,
+    yamlConsistencyJobCondition = expr { "${github.repository_owner} == 'typesafegithub' || ${github.event_name} != 'schedule'" }
+    // Inlining temporarily
+    //disableScheduledJobInForks,
 ) {
     job(
         id = "updates-available",
         runsOn = UbuntuLatest,
-        condition = disableScheduledJobInForks,
+        condition = expr { "${github.repository_owner} == 'typesafegithub' || ${github.event_name} != 'schedule'" }
+        // Inlining temporarily
+        //disableScheduledJobInForks,
     ) {
         uses(action = CheckoutV4())
-        setupJava()
+//        setupJava()
+        // Inlining temporarily
+        uses(
+            name = "Set up JDK",
+            action = SetupJavaV3(
+                javaVersion = "11",
+                distribution = SetupJavaV3.Distribution.Zulu,
+            )
+        )
         uses(
             name = "Run suggestVersions",
             env = linkedMapOf("GITHUB_TOKEN" to expr("secrets.GITHUB_TOKEN")),
