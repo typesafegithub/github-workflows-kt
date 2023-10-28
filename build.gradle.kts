94d771fd3ecd2fd292b2092398e997543e63c04f
Piotr Krzeminski <git@krzeminski.it>
2023-10-28 11:25:37 +0200

    Remove logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,6 @@ val setIsSnapshotFlagInGithubOutput by tasks.registering {
     doLast {
         val filePath = System.getenv("GITHUB_OUTPUT") ?: error("Expected GITHUB_OUTPUT variable to be set!")
         val isSnapshot = project.version.toString().endsWith("-SNAPSHOT")
-        println("Before: ${File(filePath).readText()}")
         File(filePath).appendText("is-snapshot=$isSnapshot\n")
-        println("After: ${File(filePath).readText()}")
     }
 }
