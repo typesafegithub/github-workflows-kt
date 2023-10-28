e5b905b47a5fbc4f4399f9d3b8f41575c8a250d0
Piotr Krzeminski <git@krzeminski.it>
2023-10-28 07:57:36 +0200

    testing

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -21,111 +21,12 @@ workflow(
     ),
     sourceFile = __FILE__.toPath(),
 ) {
-    listOf(UbuntuLatest, Windows2022).forEach { runnerType ->
-        job(
-            id = "build-for-${runnerType::class.simpleName}",
-            runsOn = runnerType,
-        ) {
-            uses(action = CheckoutV4())
-            setupJava()
-            uses(
-                name = "Build",
-                action = GradleBuildActionV2(
-                    arguments = "build",
-                )
-            )
-        }
-    }
-
     job(
-        id = "publish-snapshot",
-        name = "Publish snapshot",
-        runsOn = UbuntuLatest,
-        condition = expr { "${github.ref} == 'refs/heads/main'" },
-        env = linkedMapOf(
-            "SIGNING_KEY" to expr("secrets.SIGNING_KEY"),
-            "SIGNING_PASSWORD" to expr("secrets.SIGNING_PASSWORD"),
-            "ORG_GRADLE_PROJECT_sonatypeUsername" to expr("secrets.ORG_GRADLE_PROJECT_SONATYPEUSERNAME"),
-            "ORG_GRADLE_PROJECT_sonatypePassword" to expr("secrets.ORG_GRADLE_PROJECT_SONATYPEPASSWORD"),
-        ),
-    ) {
-        uses(action = CheckoutV4())
-        setupJava()
-
-        libraries.forEach { library ->
-            uses(
-                name = "Publish '$library' to Sonatype",
-                action = GradleBuildActionV2(
-                    arguments = "$library:publishToSonatype closeAndReleaseSonatypeStagingRepository",
-                ),
-            )
-        }
-    }
-
-    job(
-        id = "build_docs",
-        name = "Build docs",
+        id = "testing",
         runsOn = UbuntuLatest,
     ) {
-        uses(action = CheckoutV4())
-        setupPython()
-        run(command = "pip install -r docs/requirements.txt")
-        run(command = "mkdocs build --site-dir public")
-    }
-
-    job(
-        id = "build_kotlin_scripts",
-        name = "Build Kotlin scripts",
-        runsOn = UbuntuLatest,
-    ) {
-        uses(action = CheckoutV4())
         run(
-            name = "Generate action bindings",
-            command = ".github/workflows/generate-action-bindings.main.kts",
-        )
-        run(
-            command = """
-            find -name *.main.kts -print0 | while read -d ${'$'}'\0' file
-            do
-                echo "Compiling ${'$'}file..."
-                kotlinc -Werror -Xallow-any-scripts-in-source-roots "${'$'}file"
-            done
-            """.trimIndent()
-        )
-    }
-
-    job(
-        id = "workflows_consistency_check",
-        name = "Run consistency check on all GitHub workflows",
-        runsOn = UbuntuLatest,
-    ) {
-        uses(action = CheckoutV4())
-        uses(
-            name = "Set up Java in proper version",
-            action = SetupJavaV3(
-                javaVersion = "17",
-                distribution = SetupJavaV3.Distribution.Zulu,
-                cache = SetupJavaV3.BuildPlatform.Gradle,
-            ),
-        )
-        run(command = "cd .github/workflows")
-        run(
-            name = "Generate action bindings",
-            command = ".github/workflows/generate-action-bindings.main.kts",
-        )
-        run(
-            name = "Regenerate all workflow YAMLs",
-            command = """
-            find -name "*.main.kts" -print0 | while read -d ${'$'}'\0' file
-            do
-                echo "Regenerating ${'$'}file..."
-                (${'$'}file)
-            done
-            """.trimIndent(),
-        )
-        run(
-            name = "Check if some file is different after regeneration",
-            command = "git diff --exit-code .",
+            command = "echo ${'$'}GITHUB_OUTPUT"
         )
     }
 }.writeToFile()
