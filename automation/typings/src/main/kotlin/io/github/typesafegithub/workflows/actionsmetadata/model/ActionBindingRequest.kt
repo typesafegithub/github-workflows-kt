f746c49d133348faaa89c93b76b205a3782823b6
Piotr Krzeminski <git@krzeminski.it>
2023-11-24 20:04:49 +0100

    chore(automation): remove unused TypingsSource

--- automation/typings/src/main/kotlin/io/github/typesafegithub/workflows/actionsmetadata/model/ActionBindingRequest.kt
+++ automation/typings/src/main/kotlin/io/github/typesafegithub/workflows/actionsmetadata/model/ActionBindingRequest.kt
@@ -1,13 +1,6 @@
 package io.github.typesafegithub.workflows.actionsmetadata.model
 
 import io.github.typesafegithub.workflows.actionbindinggenerator.ActionCoords
-import io.github.typesafegithub.workflows.actionbindinggenerator.Typing
-
-sealed interface TypingsSource {
-    data class CodeGenerator(val inputTypings: Map<String, Typing> = emptyMap()) : TypingsSource
-
-    object ActionTypes : TypingsSource
-}
 
 data class ActionBindingRequest(
     val actionCoords: ActionCoords,
