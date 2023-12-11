a513fd53f1d576eedfa90b18eb1dedd83b8554f0
Piotr Krzeminski <git@krzeminski.it>
2023-12-11 14:52:25 +0100

    Revert "REVERTME: publish snapshot"
    This reverts commit 936c84a38a15c97b130da89a39c9f2b197c8b658.

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -43,6 +43,7 @@ workflow(
         id = "publish-snapshot",
         name = "Publish snapshot",
         runsOn = UbuntuLatest,
+        condition = expr { "${github.ref} == 'refs/heads/main'" },
         env = linkedMapOf(
             "SIGNING_KEY" to expr("secrets.SIGNING_KEY"),
             "SIGNING_PASSWORD" to expr("secrets.SIGNING_PASSWORD"),
@@ -62,6 +63,7 @@ workflow(
         libraries.forEach { library ->
             uses(
                 name = "Publish '$library' to Sonatype",
+                condition = expr("steps.${setIsSnapshotVersionFlag.id}.outputs.is-snapshot == 'true'"),
                 action = GradleBuildAction(
                     arguments = "$library:publishToSonatype closeAndReleaseSonatypeStagingRepository --no-configuration-cache",
                 ),
