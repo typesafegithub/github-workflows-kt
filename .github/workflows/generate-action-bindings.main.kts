c53e167876742cb2878716f0979e7a8926f6370a
Piotr Krzeminski <git@krzeminski.it>
2023-12-11 13:53:17 +0100

    REVETME: try in script

--- .github/workflows/generate-action-bindings.main.kts
+++ .github/workflows/generate-action-bindings.main.kts
@@ -1,6 +1,7 @@
 #!/usr/bin/env kotlin
-@file:DependsOn("io.github.typesafegithub:action-binding-generator:1.7.0")
+@file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+@file:DependsOn("io.github.typesafegithub:action-binding-generator:1.7.1-20231211.115819-7")
 
 import io.github.typesafegithub.workflows.actionbindinggenerator.generateActionBindings
 
-generateActionBindings(args)
+generateActionBindings(args, __FILE__.toPath())
