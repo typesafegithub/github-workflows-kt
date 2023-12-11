6519bac389a6f2bc677de44b549cac772f23610d
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2023-12-11 21:30:22 +0100

    test(abg): add smoke test for generateActionBindings (#1186)
    Part of #1071.

---
+++ action-binding-generator/src/test/kotlin/io/github/typesafegithub/workflows/actionbindinggenerator/GenerateActionBindingsCliHelperTest.kt
@@ -0,0 +1,55 @@
+package io.github.typesafegithub.workflows.actionbindinggenerator
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.engine.spec.tempdir
+import io.kotest.matchers.shouldBe
+
+class GenerateActionBindingsCliHelperTest : FunSpec({
+    test("${::generateActionBindings.name} - smoke test") {
+        // given
+        val mockGitRootDir =
+            tempdir().also {
+                it.resolve(".git").mkdir()
+                it.resolve(".github").resolve("workflows").also {
+                    it.mkdirs()
+
+                    it.resolve("_used-actions.yaml").writeText(
+                        """
+                        jobs:
+                          some-job:
+                            steps:
+                              - uses: actions/checkout@v4
+                              - uses: actions/setup-java@v4
+                              - uses: gradle/gradle-build-action@v2
+                        """.trimIndent(),
+                    )
+
+                    it.resolve("some-workflow.yaml").writeText(
+                        """
+                        jobs:
+                          some-job:
+                            steps:
+                              - uses: actions/checkout@v4
+                              - uses: gradle/gradle-build-action@v2
+                        """.trimIndent(),
+                    )
+                }
+            }
+
+        // when
+        generateActionBindings(arrayOf("some-workflow.yaml"), mockGitRootDir.toPath())
+
+        // then
+        val generatedDir = mockGitRootDir.resolve(".github").resolve("workflows").resolve("generated")
+        val generatedFiles =
+            generatedDir.walk().toList()
+                .filter { it.isFile }
+                .map { it.absoluteFile.invariantSeparatorsPath.removePrefix(mockGitRootDir.absoluteFile.invariantSeparatorsPath) }
+                .sorted()
+        generatedFiles shouldBe
+            listOf(
+                "/.github/workflows/generated/actions/checkout.kt",
+                "/.github/workflows/generated/gradle/gradle-build-action.kt",
+            )
+    }
+})
