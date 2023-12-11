706e6a45d4454706a4f422c4a44381cdbeea141c
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2023-12-11 11:48:11 +0100

    Update _shared.main.kts

--- .github/workflows/_shared.main.kts
+++ .github/workflows/_shared.main.kts
@@ -6,6 +6,10 @@ import io.github.typesafegithub.workflows.dsl.expressions.expr
 val disableScheduledJobInForks =
     expr { "${github.repository_owner} == 'typesafegithub' || ${github.event_name} != 'schedule'" }
 
+// The order is deliberate here. First, libraries with no dependencies are published.
+// Then, libraries that depend on already published libraries, and so on. Thanks to
+// such order, newly released artifacts already have their dependencies in place and
+// are ready to be used.
 val libraries = listOf(
     ":shared-internal",
     ":github-workflows-kt",
