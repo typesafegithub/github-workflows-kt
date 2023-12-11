d14fb4bffd8b9876cf1f8f8314b079eb28dd8ff9
Piotr Krzeminski <git@krzeminski.it>
2023-12-11 14:52:20 +0100

    Revert "REVETME: try in script"
    This reverts commit c53e167876742cb2878716f0979e7a8926f6370a.

--- .github/workflows/generate-action-bindings.main.kts
+++ .github/workflows/generate-action-bindings.main.kts
@@ -1,7 +1,6 @@
 #!/usr/bin/env kotlin
-@file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-@file:DependsOn("io.github.typesafegithub:action-binding-generator:1.7.1-20231211.115819-7")
+@file:DependsOn("io.github.typesafegithub:action-binding-generator:1.7.0")
 
 import io.github.typesafegithub.workflows.actionbindinggenerator.generateActionBindings
 
-generateActionBindings(args, __FILE__.toPath())
+generateActionBindings(args)
