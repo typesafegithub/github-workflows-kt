37c1d86707e8da6df12bdf97da6398050b62c633
Piotr Krzeminski <git@krzeminski.it>
2023-12-16 16:59:53 +0100

    perf(automation): generate bindings in parallel
    Implements #1191.

--- automation/code-generator/src/main/kotlin/io/github/typesafegithub/workflows/codegenerator/GenerationEntryPoint.kt
+++ automation/code-generator/src/main/kotlin/io/github/typesafegithub/workflows/codegenerator/GenerationEntryPoint.kt
@@ -1,5 +1,6 @@
 package io.github.typesafegithub.workflows.codegenerator
 
+import arrow.fx.coroutines.parMap
 import io.github.typesafegithub.workflows.actionbindinggenerator.ActionBinding
 import io.github.typesafegithub.workflows.actionbindinggenerator.FromLockfile
 import io.github.typesafegithub.workflows.actionbindinggenerator.generateBinding
@@ -12,7 +13,7 @@ import java.nio.file.Paths
 /***
  * Either run this main() function or run this command: ./gradlew :code-generator:run
  */
-fun main() {
+suspend fun main() {
     // To ensure there are no leftovers from previous generations.
     Paths.get("github-workflows-kt/src/gen").toFile().deleteRecursively()
     deleteListOfBindingsInDocs()
@@ -21,9 +22,12 @@ fun main() {
     generateListOfBindingsForDocs(requestsAndBindings)
 }
 
-private fun generateBindings(): List<Pair<ActionBindingRequest, ActionBinding>> {
+private suspend fun generateBindings(): List<Pair<ActionBindingRequest, ActionBinding>> {
     val requestsAndBindings =
-        bindingsToGenerate.map { actionBindingRequest ->
+        bindingsToGenerate.parMap(
+            // Not too much to not trigger some rate limiting on the GitHub side.
+            concurrency = 10,
+        ) { actionBindingRequest ->
             println("Generating ${actionBindingRequest.actionCoords.prettyPrint}")
             val binding = actionBindingRequest.actionCoords.generateBinding(metadataRevision = FromLockfile)
             Pair(actionBindingRequest, binding)
