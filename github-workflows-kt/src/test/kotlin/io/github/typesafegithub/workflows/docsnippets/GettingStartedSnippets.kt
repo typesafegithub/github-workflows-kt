a0241c0307066d81dd0132c11bb39f46ab8c23c5
Piotr Krzeminski <git@krzeminski.it>
2023-12-22 18:24:11 +0100

    chore: prepare for releasing version 1.8.0

--- github-workflows-kt/src/test/kotlin/io/github/typesafegithub/workflows/docsnippets/GettingStartedSnippets.kt
+++ github-workflows-kt/src/test/kotlin/io/github/typesafegithub/workflows/docsnippets/GettingStartedSnippets.kt
@@ -18,7 +18,7 @@ class GettingStartedSnippets : FunSpec({
         // --8<-- [start:getting-started-1]
         #!/usr/bin/env kotlin
 
-        @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.7.1-SNAPSHOT")
+        @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.8.0")
 
         // --8<-- [end:getting-started-1]
          */
