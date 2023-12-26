efecf6508b9c66ff00c5463fe7c353290c6fa8da
Piotr Krzeminski <git@krzeminski.it>
2023-12-26 13:58:55 +0100

    Checkout

--- .github/workflows/test_workflow.main.kts
+++ .github/workflows/test_workflow.main.kts
@@ -2,6 +2,7 @@
 @file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.8.1-20231226.125215-6")
 
+import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
 import io.github.typesafegithub.workflows.domain.RunnerType
 import io.github.typesafegithub.workflows.domain.triggers.PullRequest
 import io.github.typesafegithub.workflows.dsl.workflow
@@ -18,6 +19,7 @@ workflow(
         id = "test_job",
         runsOn = RunnerType.UbuntuLatest,
     ) {
+        uses(action = CheckoutV4())
         run(
             name = "Regular command",
             command = "echo 'Hello from regular command!'",
