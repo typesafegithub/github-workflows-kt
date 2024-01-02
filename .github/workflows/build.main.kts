883135bfa11e1bc14df0ede3eda4694a9c1b5fe7
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-01-02 22:20:50 +0100

    Apply suggestions from code review

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -43,6 +43,7 @@ workflow(
         id = "publish-snapshot",
         name = "Publish snapshot",
         runsOn = UbuntuLatest,
+        condition = expr { "${github.ref} == 'refs/heads/main'" },
         env = linkedMapOf(
             "SIGNING_KEY" to expr("secrets.SIGNING_KEY"),
             "SIGNING_PASSWORD" to expr("secrets.SIGNING_PASSWORD"),
