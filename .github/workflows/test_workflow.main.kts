6d3f9fcde85224ba3a46e2fa1296ab36d7e5118b
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-01-02 21:08:31 +0100

    Add @file:ExperimentalKotlinLogicStep

--- .github/workflows/test_workflow.main.kts
+++ .github/workflows/test_workflow.main.kts
@@ -1,12 +1,14 @@
 #!/usr/bin/env kotlin
 @file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.8.1-20240102.194124-25")
+@file:ExperimentalKotlinLogicStep
 
 import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
 import io.github.typesafegithub.workflows.domain.RunnerType
 import io.github.typesafegithub.workflows.domain.triggers.PullRequest
 import io.github.typesafegithub.workflows.dsl.workflow
 import io.github.typesafegithub.workflows.yaml.writeToFile
+import io.github.typesafegithub.workflows.annotations.ExperimentalKotlinLogicStep
 
 workflow(
     name = "Test workflow",
