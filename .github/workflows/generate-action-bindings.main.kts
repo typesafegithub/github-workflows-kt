8ee3d3405d6e6eece54971ac21af34f68906fa6e
Piotr Krzeminski <git@krzeminski.it>
2024-01-04 22:19:02 +0100

    Adjust for breaking changes

--- .github/workflows/generate-action-bindings.main.kts
+++ .github/workflows/generate-action-bindings.main.kts
@@ -1,6 +1,8 @@
 #!/usr/bin/env kotlin
 @file:DependsOn("io.github.typesafegithub:action-binding-generator:1.9.0")
 
-import io.github.typesafegithub.workflows.actionbindinggenerator.generateActionBindings
+import io.github.typesafegithub.workflows.actionbindinggenerator.annotations.ExperimentalClientSideBindings
+import io.github.typesafegithub.workflows.actionbindinggenerator.generation.generateActionBindings
 
+@OptIn(ExperimentalClientSideBindings::class)
 generateActionBindings(args = args, sourceFile = __FILE__.toPath())
