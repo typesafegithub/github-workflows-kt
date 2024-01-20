a7a8d3b5003fd983cb6b2008916bf550321583db
Piotr Krzeminski <git@krzeminski.it>
2024-01-20 10:30:48 +0100

    Update inlined binding in test

--- action-binding-generator/src/test/kotlin/io/github/typesafegithub/workflows/actionbindinggenerator/generation/GenerationTest.kt
+++ action-binding-generator/src/test/kotlin/io/github/typesafegithub/workflows/actionbindinggenerator/generation/GenerationTest.kt
@@ -468,6 +468,14 @@ class GenerationTest : FunSpec({
              * This is a test description that should be put in the KDoc comment for a class
              *
              * [Action on GitHub](https://github.com/john-smith/action-for-script)
+             *
+             * @param fooBar Short description
+             * @param bazGoo Just another input
+             * with multiline description
+             * @param _customInputs Type-unsafe map where you can put any inputs that are not yet supported by
+             * the binding
+             * @param _customVersion Allows overriding action's version, for example to use a specific minor
+             * version, or a newer version that the binding doesn't yet know about
              */
             @ExperimentalClientSideBindings
             public data class ActionForScript private constructor(
