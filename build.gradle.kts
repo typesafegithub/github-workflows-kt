3507c02baae28f734b51d3627a7b3271d16135ff
Piotr Krzeminski <git@krzeminski.it>
2024-01-20 20:07:13 +0100

    chore: prepare for releasing version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.typesafegithub"
-version = "1.9.1-SNAPSHOT"
+version = "1.10.0"
 
 nexusPublishing {
     repositories {
--- github-workflows-kt/src/test/kotlin/io/github/typesafegithub/workflows/docsnippets/GettingStartedSnippets.kt
+++ github-workflows-kt/src/test/kotlin/io/github/typesafegithub/workflows/docsnippets/GettingStartedSnippets.kt
@@ -18,7 +18,7 @@ class GettingStartedSnippets : FunSpec({
         // --8<-- [start:getting-started-1]
         #!/usr/bin/env kotlin
 
-        @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.9.1-SNAPSHOT")
+        @file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.10.0")
 
         // --8<-- [end:getting-started-1]
          */
