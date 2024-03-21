cd9254a1ae0b5b3e0faa2be68ae302205b9e7c79
Piotr Krzeminski <git@krzeminski.it>
2024-03-21 21:03:39 +0100

    Adjust

--- github-workflows-kt/src/main/kotlin/io/github/typesafegithub/workflows/dsl/HasCustomArguments.kt
+++ github-workflows-kt/src/main/kotlin/io/github/typesafegithub/workflows/dsl/HasCustomArguments.kt
@@ -3,6 +3,6 @@ package io.github.typesafegithub.workflows.dsl
 import kotlinx.serialization.Contextual
 
 public interface HasCustomArguments {
-    @Suppress("ktlint:standard:property-naming", "VariableNaming")
+    @Suppress("ktlint:standard:backing-property-naming", "VariableNaming")
     public val _customArguments: Map<String, @Contextual Any?>
 }
