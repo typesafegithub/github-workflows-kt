0620b7bea49ceecc69eec70c043d172b3d23ed80
Piotr Krzeminski <git@krzeminski.it>
2024-03-25 09:43:28 +0100

    Return JAR from the right path

--- jit-binding-server/src/main/kotlin/io/github/typesafegithub/workflows/jitbindingserver/Main.kt
+++ jit-binding-server/src/main/kotlin/io/github/typesafegithub/workflows/jitbindingserver/Main.kt
@@ -7,21 +7,26 @@ import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.response.respondOutputStream
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
         routing {
-            get("/binding/{owner}/{name}/{version}") {
+            get("/binding/{owner}/{name}/{version}/{file}") {
                 val owner = call.parameters["owner"]!!
                 val name = call.parameters["name"]!!
                 val version = call.parameters["version"]!!
-                call.respondOutputStream(
-                    contentType = ContentType.parse("application/java-archive"),
-                    status = HttpStatusCode.OK,
-                    producer = { this.buildJar(owner = owner, name = name, version = version) },
-                )
+                val file = call.parameters["file"]!!
+                when (file) {
+                    "$name-$version.jar" -> call.respondOutputStream(
+                        contentType = ContentType.parse("application/java-archive"),
+                        status = HttpStatusCode.OK,
+                        producer = { this.buildJar(owner = owner, name = name, version = version) },
+                    )
+                    else -> call.respondText(text = "Not found", status = HttpStatusCode.NotFound)
+                }
             }
         }
     }.start(wait = true)
