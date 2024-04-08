7b90b6b9028759fcef9c56cc30bb3b614137f77f
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-04-08 21:17:33 +0200

    feat(server): provide checksums for Maven artifacts (#1357)
    Part of #1318.
    
    Otherwise, Kotlin Script prints long warnings including scary
    stacktraces for each artifact, see PR description.

--- maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/VersionArtifactsBuilding.kt
+++ maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/VersionArtifactsBuilding.kt
@@ -1,5 +1,7 @@
 package io.github.typesafegithub.workflows.mavenbinding
 
+import java.security.MessageDigest
+
 sealed interface Artifact
 
 data class TextArtifact(val data: String) : Artifact
@@ -19,9 +21,24 @@ data class JarArtifact(val data: ByteArray) : Artifact {
     }
 }
 
-fun ActionCoords.buildVersionArtifacts(): Map<String, Artifact> =
-    mapOf(
-        "$name-$version.jar" to JarArtifact(buildJar(owner = owner, name = name.replace("__", "/"), version = version)),
-        "$name-$version.pom" to TextArtifact(buildPomFile(owner = owner, name = name.replace("__", "/"), version = version)),
-        "$name-$version.module" to TextArtifact(buildModuleFile(owner = owner, name = name.replace("__", "/"), version = version)),
+fun ActionCoords.buildVersionArtifacts(): Map<String, Artifact> {
+    val jar = buildJar(owner = owner, name = name.replace("__", "/"), version = version)
+    val pom = buildPomFile(owner = owner, name = name.replace("__", "/"), version = version)
+    val module = buildModuleFile(owner = owner, name = name.replace("__", "/"), version = version)
+    return mapOf(
+        "$name-$version.jar" to JarArtifact(jar),
+        "$name-$version.jar.md5" to TextArtifact(jar.md5Checksum()),
+        "$name-$version.pom" to TextArtifact(pom),
+        "$name-$version.pom.md5" to TextArtifact(pom.md5Checksum()),
+        "$name-$version.module" to TextArtifact(module),
+        "$name-$version.module.md5" to TextArtifact(module.md5Checksum()),
     )
+}
+
+private fun ByteArray.md5Checksum(): String {
+    val md5 = MessageDigest.getInstance("MD5")
+    val hashBytes = md5.digest(this)
+    return hashBytes.joinToString("") { "%02x".format(it) }
+}
+
+private fun String.md5Checksum(): String = this.toByteArray(charset = Charsets.UTF_8).md5Checksum()
