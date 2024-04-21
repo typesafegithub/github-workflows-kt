6bf5522ca5d9c46029f02d87636b5cb006318181
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-04-21 09:32:47 +0200

    Add kdoc

--- github-workflows-kt/src/main/kotlin/io/github/typesafegithub/workflows/domain/Environment.kt
+++ github-workflows-kt/src/main/kotlin/io/github/typesafegithub/workflows/domain/Environment.kt
@@ -1,5 +1,8 @@
 package io.github.typesafegithub.workflows.domain
 
+/**
+ * https://docs.github.com/en/actions/using-jobs/using-environments-for-jobs
+ */
 public data class Environment(
     val name: String,
     val url: String?,
