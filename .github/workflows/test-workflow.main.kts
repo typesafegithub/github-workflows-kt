92dfb4a283862d575f6ddbd42b6703241191e0cd
Piotr Krzeminski <git@krzeminski.it>
2024-04-22 10:45:21 +0200

    First e2e test

---
+++ .github/workflows/test-workflow.main.kts
@@ -0,0 +1,28 @@
+#!/usr/bin/env kotlin
+@file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:1.14.1-20240422.083415-28")
+@file:OptIn(ExperimentalKotlinLogicStep::class)
+
+import io.github.typesafegithub.workflows.actions.actions.CheckoutV4
+import io.github.typesafegithub.workflows.annotations.ExperimentalKotlinLogicStep
+import io.github.typesafegithub.workflows.domain.RunnerType
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.workflow
+import io.github.typesafegithub.workflows.yaml.writeToFile
+
+
+workflow(
+    name = "Build",
+    on = listOf(Push()),
+    sourceFile = __FILE__.toPath(),
+) {
+    job(
+        id = "test-job",
+        runsOn = RunnerType.UbuntuLatest,
+    ) {
+        uses(action = CheckoutV4())
+        run(name = "Step with Kotlin logic") {
+            println("Hello from Kotlin!")
+        }
+    }
+}.writeToFile()
