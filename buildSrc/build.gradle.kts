116edc645501dae677dbcaa5bef5edc7fcb6bd12
Piotr Krzeminski <git@krzeminski.it>
2024-05-07 11:10:15 +0200

    chore: update Kotlin to 2.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.24"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.0.0-RC2"))
 
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    implementation("org.jetbrains.kotlin:kotlin-serialization:1.9.24")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0-RC2")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:2.0.0-RC2")
 
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.6")
     implementation("org.jmailen.gradle:kotlinter-gradle:4.3.0")
