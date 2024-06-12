8bca33f59a105d6ef587a3b2ab599bcdf8017653
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-06-12 09:18:38 +0200

    chore: extract duplicate version checking to convention plugin (#1488)

---
+++ buildSrc/src/main/kotlin/buildsrc/convention/duplicate-versions.gradle.kts
@@ -0,0 +1,39 @@
+package buildsrc.convention
+
+import org.gradle.api.Task
+import org.gradle.kotlin.dsl.registering
+import org.gradle.kotlin.dsl.getValue
+
+val validateDuplicatedVersion by tasks.registering(Task::class) {
+    // These properties of a project need to be resolved before the 'doLast' block because otherwise, Gradle
+    // configuration cache cannot be used.
+    val rootDir = rootDir
+    val version = version
+
+    doLast {
+        require(
+            rootDir.resolve("mkdocs.yml").readText()
+                .contains("  version: $version")
+        ) { "Library version stated in the docs should be equal to $version!" }
+        require(
+            rootDir.resolve("github-workflows-kt/src/test/kotlin/io/github/typesafegithub/workflows/docsnippets/GettingStartedSnippets.kt").readText()
+                .contains("\"io.github.typesafegithub:github-workflows-kt:$version\"")
+        ) { "Library version stated in github-workflows-kt/src/test/.../GettingStarted.kt should be equal to $version!" }
+        require(
+            rootDir.resolve(".github/workflows/end-to-end-tests.main.kts").readText()
+                .contains("\"io.github.typesafegithub:github-workflows-kt:$version\"")
+        ) { "Library version stated in end-to-end-tests.main.kts should be equal to $version!" }
+        require(
+            rootDir.resolve(".github/workflows/end-to-end-tests.main.kts").readText()
+                .contains("\"io.github.typesafegithub:action-updates-checker:$version\"")
+        ) { "Library version stated in end-to-end-tests.main.kts should be equal to $version!" }
+        require(
+            rootDir.resolve("images/teaser-with-newest-version.svg").readText()
+                .contains("\"io.github.typesafegithub:github-workflows-kt:$version\"")
+        ) { "Library version stated in the teaser image shiuld be equal to $version!" }
+    }
+}
+
+project.tasks.getByName("check") {
+    dependsOn(validateDuplicatedVersion)
+}
