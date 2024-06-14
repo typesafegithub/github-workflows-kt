e57e321b41922277680c6585d1a85a939ca89d4b
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-06-14 16:48:57 +0200

    fix(jaeger): adjust Jaeger traces ingest URL (#1500)

--- jit-binding-server/src/main/kotlin/io/github/typesafegithub/workflows/jitbindingserver/OpenTelemetryConfig.kt
+++ jit-binding-server/src/main/kotlin/io/github/typesafegithub/workflows/jitbindingserver/OpenTelemetryConfig.kt
@@ -32,7 +32,7 @@ UI available at http://localhost:16686/
 */
 internal fun buildOpenTelemetryConfig(
     serviceName: String,
-    endpointConfig: String = "http://localhost:4317",
+    endpointConfig: String = "http://jaeger:4317",
 ): OpenTelemetry {
     val spanExporter =
         OtlpGrpcSpanExporter.builder()
