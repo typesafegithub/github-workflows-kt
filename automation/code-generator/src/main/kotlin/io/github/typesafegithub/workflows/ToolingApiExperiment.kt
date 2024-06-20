7bacc0ebfc7fa76f64ecce1ef01857d382482491
Piotr Krzeminski <git@krzeminski.it>
2024-06-20 19:21:45 +0200

    Print preview of task accessors

--- automation/code-generator/src/main/kotlin/io/github/typesafegithub/workflows/ToolingApiExperiment.kt
+++ automation/code-generator/src/main/kotlin/io/github/typesafegithub/workflows/ToolingApiExperiment.kt
@@ -3,6 +3,7 @@ package io.github.typesafegithub.workflows
 import org.gradle.tooling.GradleConnector
 import org.gradle.tooling.model.GradleProject
 import java.io.File
+import java.util.Locale
 
 fun main() {
     GradleConnector.newConnector()
@@ -10,11 +11,25 @@ fun main() {
         .connect()
         .use { gradleConnector ->
             val model = gradleConnector.getModel(GradleProject::class.java)
-            val projects = model.children.all
-            projects.forEach { project ->
-                project.tasks.forEach { task ->
-                    println(task)
-                }
+            val allTasks = model.tasks + model.children.all.flatMap { it.tasks }
+            allTasks.forEach { task ->
+                println(task)
+            }
+            println("Generate the following accessors:")
+            allTasks.forEach { task ->
+                println("projects" + task.path.split(":").joinToString(".") { it.toCamelCase() })
             }
         }
 }
+
+internal fun String.toPascalCase(): String {
+    val hasOnlyUppercases = none { it in 'a'..'z' }
+    val normalizedString = if (hasOnlyUppercases) lowercase() else this
+    return normalizedString.replace("+", "-plus-")
+        .split("-", "_", " ", ".", "/")
+        .joinToString("") {
+            it.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+        }
+}
+
+internal fun String.toCamelCase(): String = toPascalCase().replaceFirstChar { it.lowercase() }
