54261c82f850f5ac8d31579c090fae0a3ecc23ec
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-07-30 11:25:03 +0200

    Remove unused dependencies from automation

--- automation/code-generator/build.gradle.kts
+++ automation/code-generator/build.gradle.kts
@@ -9,15 +9,6 @@ plugins {
 dependencies {
     implementation("com.squareup:kotlinpoet:1.18.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
-
-    implementation(platform("io.ktor:ktor-bom:2.3.12"))
-    implementation("io.ktor:ktor-client-core")
-    implementation("io.ktor:ktor-client-cio")
-    implementation("io.ktor:ktor-client-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")
-
-    testImplementation(projects.githubWorkflowsKt)
 }
 
 application {
