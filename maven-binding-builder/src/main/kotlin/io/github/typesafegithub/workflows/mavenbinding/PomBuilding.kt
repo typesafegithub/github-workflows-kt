adc2fffcfdfa1de01ced1bdd0229129bcee578ff
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2024-08-05 10:22:47 +0200

    fix(server): fix URL to action repo for subactions (#1574)
    Hoping that Renovate will join PRs like
    https://github.com/typesafegithub/github-workflows-kt/pull/1572 and
    https://github.com/typesafegithub/github-workflows-kt/pull/1571

--- maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/PomBuilding.kt
+++ maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/PomBuilding.kt
@@ -6,29 +6,31 @@ internal fun buildPomFile(
     owner: String,
     name: String,
     version: String,
-): String =
-    """
-    <?xml version="1.0" encoding="UTF-8"?>
-    <project xmlns="http://maven.apache.org/POM/4.0.0" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <modelVersion>3.0/0</modelVersion>
-      <groupId>$owner</groupId>
-      <artifactId>$name</artifactId>
-      <version>$version</version>
-      <name>$name</name>
-      <description>Auto-generated binding for $owner/$name@$version.</description>
-      <url>https://github.com/$owner/$name</url>
-      <scm>
-        <connection>scm:git:git://github.com/$owner/$name.git/</connection>
-        <developerConnection>scm:git:ssh://github.com:$owner/$name.git</developerConnection>
-        <url>https://github.com/$owner/$name.git</url>
-      </scm>
-      <dependencies>
-        <dependency>
-            <groupId>io.github.typesafegithub</groupId>
-            <artifactId>github-workflows-kt</artifactId>
-            <version>$LATEST_RELASED_LIBRARY_VERSION</version>
-            <scope>compile</scope>
-        </dependency>
-      </dependencies>
-    </project>
-    """.trimIndent()
+): String {
+    val nameForRepo = name.substringBefore("/")
+    return """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <project xmlns="http://maven.apache.org/POM/4.0.0" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+          <modelVersion>3.0/0</modelVersion>
+          <groupId>$owner</groupId>
+          <artifactId>$name</artifactId>
+          <version>$version</version>
+          <name>$name</name>
+          <description>Auto-generated binding for $owner/$name@$version.</description>
+          <url>https://github.com/$owner/$nameForRepo</url>
+          <scm>
+            <connection>scm:git:git://github.com/$owner/$nameForRepo.git/</connection>
+            <developerConnection>scm:git:ssh://github.com:$owner/$nameForRepo.git</developerConnection>
+            <url>https://github.com/$owner/$nameForRepo.git</url>
+          </scm>
+          <dependencies>
+            <dependency>
+                <groupId>io.github.typesafegithub</groupId>
+                <artifactId>github-workflows-kt</artifactId>
+                <version>$LATEST_RELASED_LIBRARY_VERSION</version>
+                <scope>compile</scope>
+            </dependency>
+          </dependencies>
+        </project>
+        """.trimIndent()
+}
