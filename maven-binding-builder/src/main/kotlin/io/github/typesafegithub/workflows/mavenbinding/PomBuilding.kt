3804a2acae1ce31503d6c90173edd97626372663
Björn Kautler <Bjoern@Kautler.net>
2024-08-16 12:07:43 +0200

    feat(server): make it possible to POST custom typings for testing during typing creation

--- maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/PomBuilding.kt
+++ maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/PomBuilding.kt
@@ -11,7 +11,7 @@ internal fun ActionCoords.buildPomFile() =
     <?xml version="1.0" encoding="UTF-8"?>
     <project xmlns="http://maven.apache.org/POM/4.0.0" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
       <modelVersion>4.0.0</modelVersion>
-      <groupId>$owner</groupId>
+      <groupId>$mavenGroup</groupId>
       <artifactId>$mavenName</artifactId>
       <version>$version</version>
       <name>$fullName</name>
