7e4ff1339960cb48c661e32d12973874cd75f01d
Björn Kautler <Bjoern@Kautler.net>
2024-08-16 12:07:43 +0200

    feat(server): make it possible to POST custom typings for testing during typing creation

--- maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/BindingsServerRequest.kt
+++ maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/BindingsServerRequest.kt
@@ -3,6 +3,7 @@ package io.github.typesafegithub.workflows.mavenbinding
 import io.github.typesafegithub.workflows.actionbindinggenerator.domain.ActionCoords
 
 data class BindingsServerRequest(
+    val rawOwner: String,
     val rawName: String,
     val rawVersion: String?,
     val actionCoords: ActionCoords,
