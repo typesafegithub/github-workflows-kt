c70098e4b76c833f71a7b532bc4bf5a5634de3f9
Piotr Krzeminski <git@krzeminski.it>
2024-08-17 23:30:08 +0200

    test: check if untyped action has untyped binding

--- .github/workflows/test-script-consuming-jit-bindings.main.do-not-compile.kts
+++ .github/workflows/test-script-consuming-jit-bindings.main.do-not-compile.kts
@@ -13,12 +13,17 @@
 // Using specific version.
 @file:DependsOn("actions:cache:v3.3.3")
 
+// Always untyped action.
+@file:DependsOn("typesafegithub:always-untyped-action-for-tests:v1")
+
 import io.github.typesafegithub.workflows.actions.actions.Cache
 import io.github.typesafegithub.workflows.actions.actions.Checkout
 import io.github.typesafegithub.workflows.actions.actions.Checkout_Untyped
 import io.github.typesafegithub.workflows.actions.gradle.ActionsSetupGradle
+import io.github.typesafegithub.workflows.actions.typesafegithub.AlwaysUntypedActionForTests_Untyped
 
 println(Checkout_Untyped(fetchTags = "false"))
 println(Checkout(fetchTags = false))
+println(AlwaysUntypedActionForTests_Untyped(foobar = "baz"))
 println(ActionsSetupGradle())
 println(Cache(path = listOf("some-path"), key = "some-key"))
