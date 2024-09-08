16927403134762d1e50cb9b5d479a8bbfe6c2e66
Björn Kautler <Bjoern@Kautler.net>
2024-09-08 04:30:31 +0200

    feat(abg): add binding version v2

--- action-binding-generator/src/test/kotlin/io/github/typesafegithub/workflows/actionbindinggenerator/bindingsfromunittests/ActionWithPartlyTypingsBindingV1.kt
+++ action-binding-generator/src/test/kotlin/io/github/typesafegithub/workflows/actionbindinggenerator/bindingsfromunittests/ActionWithPartlyTypingsBindingV1.kt
@@ -12,6 +12,7 @@ import io.github.typesafegithub.workflows.domain.actions.Action
 import io.github.typesafegithub.workflows.domain.actions.RegularAction
 import java.util.LinkedHashMap
 import kotlin.ExposedCopyVisibility
+import kotlin.Int
 import kotlin.String
 import kotlin.Suppress
 import kotlin.Unit
@@ -20,27 +21,29 @@ import kotlin.collections.toList
 import kotlin.collections.toTypedArray
 
 /**
- * Action: Some Action
+ * Action: Do something cool
  *
- * Description
+ * This is a test description that should be put in the KDoc comment for a class
  *
- * [Action on GitHub](https://github.com/john-smith/action-with-deprecated-input-and-name-clash)
+ * [Action on GitHub](https://github.com/john-smith/action-with-partly-typings-binding-v1)
  *
- * @param fooBar &lt;required&gt; Foo bar - new
- * @param fooBar_Untyped &lt;required&gt; Foo bar - new
+ * @param foo &lt;required&gt;
+ * @param foo_Untyped &lt;required&gt;
  * @param _customInputs Type-unsafe map where you can put any inputs that are not yet supported by the binding
  * @param _customVersion Allows overriding action's version, for example to use a specific minor version, or a newer version that the binding doesn't yet know about
  */
 @ExposedCopyVisibility
-public data class ActionWithDeprecatedInputAndNameClash private constructor(
+public data class ActionWithPartlyTypingsBindingV1 private constructor(
     /**
-     * &lt;required&gt; Foo bar - new
+     * &lt;required&gt;
      */
-    public val fooBar: String? = null,
+    public val foo: Int? = null,
     /**
-     * &lt;required&gt; Foo bar - new
+     * &lt;required&gt;
      */
-    public val fooBar_Untyped: String? = null,
+    public val foo_Untyped: String? = null,
+    public val bar_Untyped: String? = null,
+    public val baz_Untyped: String,
     /**
      * Type-unsafe map where you can put any inputs that are not yet supported by the binding
      */
@@ -49,29 +52,33 @@ public data class ActionWithDeprecatedInputAndNameClash private constructor(
      * Allows overriding action's version, for example to use a specific minor version, or a newer version that the binding doesn't yet know about
      */
     public val _customVersion: String? = null,
-) : RegularAction<Action.Outputs>("john-smith", "action-with-deprecated-input-and-name-clash", _customVersion ?: "v2") {
+) : RegularAction<Action.Outputs>("john-smith", "action-with-partly-typings-binding-v1", _customVersion ?: "v3") {
     init {
-        require(!((fooBar != null) && (fooBar_Untyped != null))) {
-            "Only fooBar or fooBar_Untyped must be set, but not both"
+        require(!((foo != null) && (foo_Untyped != null))) {
+            "Only foo or foo_Untyped must be set, but not both"
         }
-        require((fooBar != null) || (fooBar_Untyped != null)) {
-            "Either fooBar or fooBar_Untyped must be set, one of them is required"
+        require((foo != null) || (foo_Untyped != null)) {
+            "Either foo or foo_Untyped must be set, one of them is required"
         }
     }
 
     public constructor(
         vararg pleaseUseNamedArguments: Unit,
-        fooBar: String? = null,
-        fooBar_Untyped: String? = null,
+        foo: Int? = null,
+        foo_Untyped: String? = null,
+        bar_Untyped: String? = null,
+        baz_Untyped: String,
         _customInputs: Map<String, String> = mapOf(),
         _customVersion: String? = null,
-    ) : this(fooBar = fooBar, fooBar_Untyped = fooBar_Untyped, _customInputs = _customInputs, _customVersion = _customVersion)
+    ) : this(foo = foo, foo_Untyped = foo_Untyped, bar_Untyped = bar_Untyped, baz_Untyped = baz_Untyped, _customInputs = _customInputs, _customVersion = _customVersion)
 
     @Suppress("SpreadOperator")
     override fun toYamlArguments(): LinkedHashMap<String, String> = linkedMapOf(
         *listOfNotNull(
-            fooBar?.let { "fooBar" to it },
-            fooBar_Untyped?.let { "fooBar" to it },
+            foo?.let { "foo" to it.toString() },
+            foo_Untyped?.let { "foo" to it },
+            bar_Untyped?.let { "bar" to it },
+            "baz" to baz_Untyped,
             *_customInputs.toList().toTypedArray(),
         ).toTypedArray()
     )
