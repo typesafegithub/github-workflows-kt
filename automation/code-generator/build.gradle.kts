8d1390a57c5f8bcde7e17c4eaac8060e35538745
Piotr Krzeminski <git@krzeminski.it>
2024-10-31 18:23:44 +0100

    First iteration

--- automation/code-generator/build.gradle.kts
+++ automation/code-generator/build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
 }
 
 dependencies {
+    implementation("com.google.devtools.ksp:symbol-processing-api:2.0.21-1.0.26")
     implementation("com.squareup:kotlinpoet:2.0.0")
+    implementation("com.squareup:kotlinpoet-ksp:2.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
 
