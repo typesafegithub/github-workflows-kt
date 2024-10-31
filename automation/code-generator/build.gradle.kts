3dcd388131884d129705e8d4538db878d5579305
Piotr Krzeminski <git@krzeminski.it>
2024-10-31 18:29:48 +0100

    Remove application plugin and config

--- automation/code-generator/build.gradle.kts
+++ automation/code-generator/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     buildsrc.convention.`kotlin-jvm`
 
     kotlin("plugin.serialization")
-
-    application
 }
 
 dependencies {
@@ -11,12 +9,4 @@ dependencies {
     implementation("com.squareup:kotlinpoet:2.0.0")
     implementation("com.squareup:kotlinpoet-ksp:2.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-}
-
-application {
-    mainClass.set("io.github.typesafegithub.workflows.codegenerator.GenerationEntryPointKt")
-}
-
-tasks.run.configure {
-    workingDir(rootProject.layout.projectDirectory)
 }
