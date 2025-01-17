9639fd4f9c16b87c72bc2673930c982ccdccec97
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-01-17 10:40:03 +0100

    fix(server): return 'not found' for maven-metadata.xml if no major version found (#1775)
    Part of
    https://github.com/typesafegithub/github-workflows-kt/issues/1774.
    
    It doesn't fix the entire issue because we should be able to see the
    major versions, even if they don't have the `v` in front, because such
    versions are consumable as action bindings. This change just prevents
    the failures of the server.

--- maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/PackageArtifactsBuilding.kt
+++ maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/PackageArtifactsBuilding.kt
@@ -3,7 +3,7 @@ package io.github.typesafegithub.workflows.mavenbinding
 import io.github.typesafegithub.workflows.actionbindinggenerator.domain.ActionCoords
 
 suspend fun ActionCoords.buildPackageArtifacts(githubToken: String): Map<String, String> {
-    val mavenMetadata = buildMavenMetadataFile(githubToken = githubToken)
+    val mavenMetadata = buildMavenMetadataFile(githubToken = githubToken) ?: return emptyMap()
     return mapOf(
         "maven-metadata.xml" to mavenMetadata,
         "maven-metadata.xml.md5" to mavenMetadata.md5Checksum(),
