8c70dbb8279caf13e5288d85492d9e33fb415750
Björn Kautler <Bjoern@Kautler.net>
2025-01-27 14:03:32 +0100

    Also make it possible to supply the action manifest

--- jit-binding-server/src/main/kotlin/io/github/typesafegithub/workflows/jitbindingserver/ArtifactRoutes.kt
+++ jit-binding-server/src/main/kotlin/io/github/typesafegithub/workflows/jitbindingserver/ArtifactRoutes.kt
@@ -12,8 +12,11 @@ import io.github.typesafegithub.workflows.mavenbinding.TextArtifact
 import io.github.typesafegithub.workflows.mavenbinding.buildVersionArtifacts
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.PartData
+import io.ktor.http.content.asFlow
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.request.httpMethod
+import io.ktor.server.request.receiveMultipart
 import io.ktor.server.request.receiveText
 import io.ktor.server.response.respondBytes
 import io.ktor.server.response.respondText
@@ -23,12 +26,19 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.head
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import io.ktor.utils.io.readRemaining
+import io.ktor.utils.io.readText
 import io.micrometer.core.instrument.Tag
 import io.micrometer.core.instrument.binder.cache.CaffeineCacheMetrics
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import it.krzeminski.snakeyaml.engine.kmp.api.Load
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 import java.util.UUID.randomUUID
 import kotlin.time.Duration.Companion.hours
+
+private const val METADATA_PARAMETER = "actionYaml"
+private const val TYPES_PARAMETER = "types"
 
 private val logger = logger { }
 
@@ -112,27 +122,113 @@ private fun Route.postArtifact(prometheusRegistry: PrometheusMeterRegistry) {
         val owner = "${call.parameters["owner"]}__types__${randomUUID()}"
         val name = call.parameters["name"]!!
         val version = call.parameters["version"]!!
-        val types = call.receiveText()
-        runCatching {
-            Load().loadOne(types)
-        }.onFailure {
-            call.respondText(
-                text = "Exception while parsing supplied typings:\n${it.stackTraceToString()}",
-                status = HttpStatusCode.UnprocessableEntity,
-            )
+
+        val (metadata, types) =
+            runCatching {
+                val parts =
+                    call
+                        .receiveMultipart()
+                        .asFlow()
+                        .map { it.name to it.asString() }
+                        .toList()
+                        .map { (name, result) ->
+                            name to
+                                when {
+                                    result.isSuccess -> result.getOrThrow()
+                                    else -> {
+                                        call.respondText(
+                                            text = HttpStatusCode.InternalServerError.description,
+                                            status = HttpStatusCode.InternalServerError,
+                                        )
+                                        return@post
+                                    }
+                                }
+                        }.associate { it }
+
+                if (parts.keys.any { (it != METADATA_PARAMETER) && (it != TYPES_PARAMETER) }) {
+                    call.respondText(
+                        text = "Only '$METADATA_PARAMETER' and '$TYPES_PARAMETER' are allowed as form data fields",
+                        status = HttpStatusCode.BadRequest,
+                    )
+                    return@post
+                }
+                if (!parts.containsKey(TYPES_PARAMETER)) {
+                    call.respondText(
+                        text = "'$TYPES_PARAMETER' field is mandatory",
+                        status = HttpStatusCode.BadRequest,
+                    )
+                    return@post
+                }
+                parts[METADATA_PARAMETER] to parts[TYPES_PARAMETER]!!
+            }.recover {
+                null to call.receiveText()
+            }.getOrThrow()
+
+        if (!call.validateMetadata(metadata) || !call.validateTypes(types)) {
             return@post
         }
-        call.toBindingArtifacts(refresh = true, owner = owner, types = types)
+
+        call.toBindingArtifacts(refresh = true, owner = owner, types = types, metadata = metadata)
         call.respondText(text = "$owner:$name:$version")
 
         incrementArtifactCounter(prometheusRegistry, call)
     }
 }
 
+private suspend fun PartData.asString() =
+    runCatching {
+        when (this) {
+            is PartData.FileItem -> provider().readRemaining().readText()
+            is PartData.FormItem -> value
+            else -> {
+                logger.error { "Unexpected part data ${this::class.simpleName}" }
+                error("Unexpected part data ${this::class.simpleName}")
+            }
+        }
+    }
+
+private suspend fun ApplicationCall.validateTypes(types: String): Boolean {
+    runCatching {
+        Load().loadOne(types)
+    }.onFailure {
+        respondText(
+            text = "Exception while parsing supplied $TYPES_PARAMETER:\n${it.stackTraceToString()}",
+            status = HttpStatusCode.UnprocessableEntity,
+        )
+        return false
+    }
+    return true
+}
+
+private suspend fun ApplicationCall.validateMetadata(metadata: String?): Boolean {
+    var result = true
+    if (metadata != null) {
+        if (metadata.isEmpty()) {
+            respondText(
+                text = "Supplied $METADATA_PARAMETER is empty",
+                status = HttpStatusCode.UnprocessableEntity,
+            )
+            result = false
+        }
+
+        runCatching {
+            Load().loadOne(metadata)
+        }.onFailure {
+            respondText(
+                text = "Exception while parsing supplied $METADATA_PARAMETER:\n${it.stackTraceToString()}",
+                status = HttpStatusCode.UnprocessableEntity,
+            )
+            result = false
+        }
+    }
+    return result
+}
+
 private suspend fun ApplicationCall.toBindingArtifacts(
     refresh: Boolean,
     owner: String = parameters["owner"]!!,
     types: String? = null,
+    metadata: String? = null,
 ): Map<String, Artifact>? {
     val actionCoords = parameters.extractActionCoords(extractVersion = true, owner = owner)
 
@@ -142,7 +238,7 @@ private suspend fun ApplicationCall.toBindingArtifacts(
     }
     return (
         if (types != null) {
-            bindingsCache.get(actionCoords) { runCatching { actionCoords.buildVersionArtifacts(types)!! } }
+            bindingsCache.get(actionCoords) { runCatching { actionCoords.buildVersionArtifacts(types, metadata)!! } }
         } else {
             bindingsCache.get(actionCoords)
         }
