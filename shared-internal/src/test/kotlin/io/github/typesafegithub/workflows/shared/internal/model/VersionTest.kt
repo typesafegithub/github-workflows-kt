e8dbda38d79b80fb79a83b302845bf31969401fe
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-02-21 12:05:42 +0100

    test: add test for parsing version components (#1833)
    It reveals a bug found by @Vampire and being fixed in
    https://github.com/typesafegithub/github-workflows-kt/pull/1831. This PR
    adds proper, direct visibility to the current behavior.

--- shared-internal/src/test/kotlin/io/github/typesafegithub/workflows/shared/internal/model/VersionTest.kt
+++ shared-internal/src/test/kotlin/io/github/typesafegithub/workflows/shared/internal/model/VersionTest.kt
@@ -1,10 +1,34 @@
 package io.github.typesafegithub.workflows.shared.internal.model
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class VersionTest :
     FunSpec({
+        context("parsing") {
+            listOf(
+                Pair("v1.2.3", Triple(1, 2, 3)),
+                Pair("1.2.3", Triple(1, 2, 3)),
+                Pair("v1.2.3.4", Triple(1, 2, 4)),
+                Pair("1.2.3.4", Triple(1, 2, 4)),
+                Pair("v1.2", Triple(1, 2, 2)),
+                Pair("v3", Triple(3, 3, 3)),
+                Pair("V3", Triple(3, 3, 3)),
+                Pair("v3-prerelease", Triple(0, 0, 0)),
+                Pair("beta-v3", Triple(0, 0, 0)),
+            ).forEach { (string, version) ->
+                test("correctly parses $string") {
+                    val parsedVersion = Version(string)
+                    assertSoftly {
+                        parsedVersion.major shouldBe version.first
+                        parsedVersion.minor shouldBe version.second
+                        parsedVersion.patch shouldBe version.third
+                    }
+                }
+            }
+        }
+
         context("isMajorVersion") {
             listOf(
                 Pair("v1.2.3", false),
