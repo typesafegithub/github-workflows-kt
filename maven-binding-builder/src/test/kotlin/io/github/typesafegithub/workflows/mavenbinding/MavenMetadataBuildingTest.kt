bb270d4afc57ec904023aa7c93428567510c74e7
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-02-24 10:17:55 +0100

    test(server): add tests for Maven metadata building (#1837)
    Related to
    https://github.com/typesafegithub/github-workflows-kt/pull/1611.
    
    We're going to make some changes in the function, so let's add some
    safety net to prevent regressions + a way to see how the modified
    function behaves.
    
    ---------
    
    Co-authored-by: Björn Kautler <Bjoern@Kautler.net>

---
+++ maven-binding-builder/src/test/kotlin/io/github/typesafegithub/workflows/mavenbinding/MavenMetadataBuildingTest.kt
@@ -0,0 +1,94 @@
+package io.github.typesafegithub.workflows.mavenbinding
+
+import io.github.typesafegithub.workflows.actionbindinggenerator.domain.ActionCoords
+import io.github.typesafegithub.workflows.shared.internal.model.Version
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import java.time.ZonedDateTime
+
+class MavenMetadataBuildingTest :
+    FunSpec({
+        val actionCoords =
+            ActionCoords(
+                owner = "owner",
+                name = "name",
+                version = "irrelevant",
+            )
+
+        test("various kinds of versions available") {
+            // Given
+            val fetchAvailableVersions: suspend (String, String, String?) -> List<Version> = { _, _, _ ->
+                listOf(
+                    Version(version = "v3-beta", dateProvider = { ZonedDateTime.parse("2024-07-01T00:00:00Z") }),
+                    Version(version = "v2", dateProvider = { ZonedDateTime.parse("2024-05-01T00:00:00Z") }),
+                    Version(version = "v1", dateProvider = { ZonedDateTime.parse("2024-03-07T00:00:00Z") }),
+                    Version(version = "v1.1", dateProvider = { ZonedDateTime.parse("2024-03-07T00:00:00Z") }),
+                    Version(version = "v1.1.0", dateProvider = { ZonedDateTime.parse("2024-03-07T00:00:00Z") }),
+                    Version(version = "v1.0.1", dateProvider = { ZonedDateTime.parse("2024-03-05T00:00:00Z") }),
+                    Version(version = "v1.0", dateProvider = { ZonedDateTime.parse("2024-03-01T00:00:00Z") }),
+                    Version(version = "v1.0.0", dateProvider = { ZonedDateTime.parse("2024-03-01T00:00:00Z") }),
+                )
+            }
+
+            val xml =
+                actionCoords.buildMavenMetadataFile(
+                    githubToken = "SOME_TOKEN",
+                    fetchAvailableVersions = fetchAvailableVersions,
+                )
+
+            xml shouldBe
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <metadata>
+                  <groupId>owner</groupId>
+                  <artifactId>name</artifactId>
+                  <versioning>
+                    <latest>v2</latest>
+                    <release>v2</release>
+                    <versions>
+                      <version>v2</version>
+                      <version>v1</version>
+                    </versions>
+                    <lastUpdated>20240501000000</lastUpdated>
+                  </versioning>
+                </metadata>
+                """.trimIndent()
+        }
+
+        test("no major versions") {
+            // Given
+            val fetchAvailableVersions: suspend (String, String, String?) -> List<Version> = { _, _, _ ->
+                listOf(
+                    Version(version = "v1.1", dateProvider = { ZonedDateTime.parse("2024-03-07T00:00:00Z") }),
+                    Version(version = "v1.1.0", dateProvider = { ZonedDateTime.parse("2024-03-07T00:00:00Z") }),
+                    Version(version = "v1.0.1", dateProvider = { ZonedDateTime.parse("2024-03-05T00:00:00Z") }),
+                    Version(version = "v1.0", dateProvider = { ZonedDateTime.parse("2024-03-01T00:00:00Z") }),
+                    Version(version = "v1.0.0", dateProvider = { ZonedDateTime.parse("2024-03-01T00:00:00Z") }),
+                )
+            }
+
+            val xml =
+                actionCoords.buildMavenMetadataFile(
+                    githubToken = "SOME_TOKEN",
+                    fetchAvailableVersions = fetchAvailableVersions,
+                )
+
+            xml.shouldBeNull()
+        }
+
+        test("no versions available") {
+            // Given
+            val fetchAvailableVersions: suspend (String, String, String?) -> List<Version> = { _, _, _ ->
+                emptyList()
+            }
+
+            val xml =
+                actionCoords.buildMavenMetadataFile(
+                    githubToken = "SOME_TOKEN",
+                    fetchAvailableVersions = fetchAvailableVersions,
+                )
+
+            xml.shouldBeNull()
+        }
+    })
