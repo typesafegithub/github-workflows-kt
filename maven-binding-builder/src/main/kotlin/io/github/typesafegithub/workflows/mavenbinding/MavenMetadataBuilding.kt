f3033f4149366578ea6a825713a0779bde7b47c0
Piotr Krzeminski <git@krzeminski.it>
2025-02-24 08:12:04 +0100

    test(server): add tests for Maven metadata building

--- maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/MavenMetadataBuilding.kt
+++ maven-binding-builder/src/main/kotlin/io/github/typesafegithub/workflows/mavenbinding/MavenMetadataBuilding.kt
@@ -2,11 +2,15 @@ package io.github.typesafegithub.workflows.mavenbinding
 
 import io.github.typesafegithub.workflows.actionbindinggenerator.domain.ActionCoords
 import io.github.typesafegithub.workflows.shared.internal.fetchAvailableVersions
+import io.github.typesafegithub.workflows.shared.internal.model.Version
 import java.time.format.DateTimeFormatter
 
-internal suspend fun ActionCoords.buildMavenMetadataFile(githubToken: String): String? {
+internal suspend fun ActionCoords.buildMavenMetadataFile(
+    githubToken: String,
+    fetchAvailableVersions: suspend (owner: String, name: String, githubToken: String?) -> List<Version> = ::fetchAvailableVersions,
+): String? {
     val availableMajorVersions =
-        fetchAvailableVersions(owner = owner, name = name, githubToken = githubToken)
+        fetchAvailableVersions(owner, name, githubToken)
             .filter { it.isMajorVersion() }
     val newest = availableMajorVersions.maxOrNull() ?: return null
     val lastUpdated =
