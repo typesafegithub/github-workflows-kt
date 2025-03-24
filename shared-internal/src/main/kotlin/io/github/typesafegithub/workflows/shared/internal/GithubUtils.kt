df53bd8b773d23e47c34984859f19930767a4de9
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2025-03-24 19:47:05 -0300

    fix(server): fallback to GITHUB_TOKEN for app access token retrieval

--- shared-internal/src/main/kotlin/io/github/typesafegithub/workflows/shared/internal/GithubUtils.kt
+++ shared-internal/src/main/kotlin/io/github/typesafegithub/workflows/shared/internal/GithubUtils.kt
@@ -3,6 +3,6 @@ package io.github.typesafegithub.workflows.shared.internal
 import kotlinx.coroutines.runBlocking
 
 
-fun getAppAccessToken(): String = runBlocking { getInstallationAccessToken() }
+fun getAppAccessToken(): String = runBlocking { System.getenv("GITHUB_TOKEN") ?: getInstallationAccessToken() }
 
 fun getAppAccessTokenOrNull(): String? = runCatching { getAppAccessToken() }.getOrNull()
