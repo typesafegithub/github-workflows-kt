3b4d11b69db37fdbc3c14664a8013a582f1754a6
Piotr Krzeminski <git@krzeminski.it>
2025-03-25 13:23:58 +0100

    Nitpicking

--- shared-internal/src/main/kotlin/io/github/typesafegithub/workflows/shared/internal/GitHubApp.kt
+++ shared-internal/src/main/kotlin/io/github/typesafegithub/workflows/shared/internal/GitHubApp.kt
@@ -18,19 +18,34 @@ import java.time.Instant
 import java.time.ZonedDateTime
 import java.util.Base64
 
+suspend fun getInstallationAccessToken(): String? {
+    if (cachedAccessToken?.isExpired() == false) return cachedAccessToken!!.token
+    val jwtToken = generateJWTToken() ?: return null
+    cachedAccessToken =
+        httpClient
+            .post("https://api.github.com/app/installations/$INSTALLATION_ID/access_tokens") {
+                header("Accept", "application/vnd.github+json")
+                header("Authorization", "Bearer $jwtToken")
+                header("X-GitHub-Api-Version", "2022-11-28")
+            }.body()
+    return cachedAccessToken!!.token
+}
+
+private const val INSTALLATION_ID = "62885502"
+
 private var cachedAccessToken: Token? = null
 
 @Serializable
-data class Token(
+private data class Token(
     val token: String,
     val expires_at: String,
     val permissions: Map<String, String>,
     val repository_selection: String,
 ) {
     @Transient
-    private val expiresAtDate = ZonedDateTime.parse(expires_at)
+    private val expiresAtDateTime = ZonedDateTime.parse(expires_at)
 
-    fun isExpired() = expiresAtDate.isBefore(ZonedDateTime.now())
+    fun isExpired() = ZonedDateTime.now().isAfter(expiresAtDateTime)
 }
 
 private val httpClient =
@@ -42,33 +57,22 @@ private val httpClient =
         }
     }
 
-suspend fun getInstallationAccessToken(installationId: String = "62885502"): String? {
-    if (cachedAccessToken?.isExpired() == false) return cachedAccessToken!!.token
-    val jwtToken = generateJWTToken() ?: return null
-    cachedAccessToken =
-        httpClient
-            .post("https://api.github.com/app/installations/$installationId/access_tokens") {
-                header("Accept", "application/vnd.github+json")
-                header("Authorization", "Bearer $jwtToken")
-                header("X-GitHub-Api-Version", "2022-11-28")
-            }.body()
-    return cachedAccessToken!!.token
-}
+private const val GITHUB_CLIENT_ID = "Iv23liIZ17VJKUpjacBs"
 
-private fun generateJWTToken(githubClientId: String = "Iv23liIZ17VJKUpjacBs"): String? {
+private fun generateJWTToken(): String? {
     val key = loadRsaKey() ?: return null
     val algorithm = Algorithm.RSA256(null, key)
     val now = Instant.now()
     return JWT
         .create()
-        .withIssuer(githubClientId)
+        .withIssuer(GITHUB_CLIENT_ID)
         .withIssuedAt(now.minusMinutes(1))
         .withExpiresAt(now.plusMinutes(9))
         .sign(algorithm)
 }
 
-private fun loadRsaKey(privateKey: String? = System.getenv("APP_PRIVATE_KEY")): RSAPrivateKey? {
-    if (privateKey == null) return null
+private fun loadRsaKey(): RSAPrivateKey? {
+    val privateKey = System.getenv("APP_PRIVATE_KEY") ?: return null
     val filtered =
         privateKey
             .replace("-----BEGIN PRIVATE KEY-----", "")
