da344cb08cd58170a6d180afd7cf3416fb385757
Piotr Krzeminski <git@krzeminski.it>
2025-03-25 13:32:08 +0100

    Refine error message if no config

--- shared-internal/src/main/kotlin/io/github/typesafegithub/workflows/shared/internal/GithubUtils.kt
+++ shared-internal/src/main/kotlin/io/github/typesafegithub/workflows/shared/internal/GithubUtils.kt
@@ -13,13 +13,7 @@ fun getGithubAuthTokenOrNull(): String? =
         (System.getenv("GITHUB_TOKEN") ?: getInstallationAccessToken())
             .also {
                 if (it == null) {
-                    println(
-                        """
-                        Missing environment variable export GITHUB_TOKEN=token
-                        Create a personal token at https://github.com/settings/tokens
-                        The token needs to have public_repo scope.
-                        """.trimIndent(),
-                    )
+                    println(ERROR_NO_CONFIGURATION)
                 }
             }
     }
@@ -30,12 +24,14 @@ fun getGithubAuthTokenOrNull(): String? =
  * The token may be of various kind, e.g. a Personal Access Token, or an
  * Application Installation Token.
  */
-fun getGithubAuthToken(): String =
-    getGithubAuthTokenOrNull()
-        ?: error(
-            """
-            Missing environment variable export GITHUB_TOKEN=token
-            Create a personal token at https://github.com/settings/tokens
-            The token needs to have public_repo scope.
-            """.trimIndent(),
-        )
+fun getGithubAuthToken(): String = getGithubAuthTokenOrNull() ?: error(ERROR_NO_CONFIGURATION)
+
+private val ERROR_NO_CONFIGURATION =
+    """
+    Missing environment variables for generating an auth token. There are two options:
+    1. Create a personal access token at https://github.com/settings/tokens.
+       The token needs to have public_repo scope. Then, set it in `GITHUB_TOKEN` env var.
+       With this approach, listing versions for some actions may not work.
+    2. Create a GitHub app, and generate a private key. Then, set it in `APP_PRIVATE_KEY` env var.
+       With this approach, listing versions for all actions works.
+    """.trimIndent()
