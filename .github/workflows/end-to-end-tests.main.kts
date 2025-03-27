1a4225b204dc7c09e5694462426f630f1ce1dc83
Piotr Krzeminski <git@krzeminski.it>
2025-03-27 12:29:43 +0100

    chore: prepare for next dev cycle

--- .github/workflows/end-to-end-tests.main.kts
+++ .github/workflows/end-to-end-tests.main.kts
@@ -1,7 +1,7 @@
 #!/usr/bin/env kotlin
 @file:Repository("file://~/.m2/repository/")
-@file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.3.0")
-@file:DependsOn("io.github.typesafegithub:action-updates-checker:3.3.0")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.3.1-SNAPSHOT")
+@file:DependsOn("io.github.typesafegithub:action-updates-checker:3.3.1-SNAPSHOT")
 @file:Repository("https://bindings.krzeminski.it")
 @file:DependsOn("actions:checkout:v4")
 @file:DependsOn("actions:github-script:v7")
