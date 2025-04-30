76129a216a6296bc8fbf4fee8f3d479e20232e12
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-04-30 23:14:46 +0200

    test(server): add more tests with mocked versioned artifact building (#1931)
    So that we don't rely solely on the end-to-end tests that call the
    server via the Kotlin script.
    Having such tests should give us a shorter feedback loop via Gradle if
    there's an obvious regression.

--- jit-binding-server/src/test/kotlin/io/github/typesafegithub/workflows/jitbindingserver/ArtifactRoutesTest.kt
+++ jit-binding-server/src/test/kotlin/io/github/typesafegithub/workflows/jitbindingserver/ArtifactRoutesTest.kt
@@ -1,8 +1,10 @@
 package io.github.typesafegithub.workflows.jitbindingserver
 
+import io.github.typesafegithub.workflows.mavenbinding.TextArtifact
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.routing.routing
 import io.ktor.server.testing.testApplication
@@ -10,6 +12,51 @@ import io.ktor.server.testing.testApplication
 class ArtifactRoutesTest :
     FunSpec({
         context("artifacts for a given version") {
+            test("when some artifacts were generated") {
+                testApplication {
+                    // Given
+                    application {
+                        routing {
+                            artifactRoutes(
+                                buildBindingsCache(
+                                    buildVersionArtifacts = {
+                                        mapOf("some-action-v4.pom" to TextArtifact { "Some POM contents" })
+                                    },
+                                ),
+                            )
+                        }
+                    }
+
+                    // When
+                    val response = client.get("some-owner/some-action/v4/some-action-v4.pom")
+
+                    // Then
+                    response.status shouldBe HttpStatusCode.OK
+                    response.bodyAsText() shouldBe "Some POM contents"
+                }
+            }
+
+            test("when no artifacts could be generated") {
+                testApplication {
+                    // Given
+                    application {
+                        routing {
+                            artifactRoutes(
+                                buildBindingsCache(
+                                    buildVersionArtifacts = { null },
+                                ),
+                            )
+                        }
+                    }
+
+                    // When
+                    val response = client.get("some-owner/some-action/v4/some-action-v4.pom")
+
+                    // Then
+                    response.status shouldBe HttpStatusCode.NotFound
+                }
+            }
+
             test("when binding generation fails") {
                 testApplication {
                     // Given
