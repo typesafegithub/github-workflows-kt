737380d112cdd8224c659f0c5f6c265a680cc817
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-05-02 21:11:48 +0200

    test(server): add a reproducing test for failure being cached (#1939)
    Part of
    https://github.com/typesafegithub/github-workflows-kt/issues/1938.
    
    Adds a test that captures undesired behavior.

--- jit-binding-server/src/test/kotlin/io/github/typesafegithub/workflows/jitbindingserver/ArtifactRoutesTest.kt
+++ jit-binding-server/src/test/kotlin/io/github/typesafegithub/workflows/jitbindingserver/ArtifactRoutesTest.kt
@@ -1,5 +1,6 @@
 package io.github.typesafegithub.workflows.jitbindingserver
 
+import io.github.typesafegithub.workflows.actionbindinggenerator.domain.ActionCoords
 import io.github.typesafegithub.workflows.mavenbinding.TextArtifact
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
@@ -7,6 +8,9 @@ import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 
 class ArtifactRoutesTest :
     FunSpec({
@@ -62,6 +66,39 @@ class ArtifactRoutesTest :
 
                     // Then
                     response.status shouldBe HttpStatusCode.InternalServerError
+                }
+            }
+
+            test("when binding generation fails and then succeeds, and two requests are made") {
+                testApplication {
+                    // Given
+                    val mockBuildVersionArtifacts = mockk<(ActionCoords) -> Map<String, TextArtifact>?>()
+                    every { mockBuildVersionArtifacts(any()) } throws
+                        Exception("An internal error occurred!") andThen
+                        mapOf("some-action-v4.pom" to TextArtifact { "Some POM contents" })
+                    application {
+                        appModule(
+                            buildVersionArtifacts = mockBuildVersionArtifacts,
+                        )
+                    }
+
+                    // When
+                    val response = client.get("some-owner/some-action/v4/some-action-v4.pom")
+                    // Then
+                    response.status shouldBe HttpStatusCode.InternalServerError
+
+                    // When
+                    val response2 = client.get("some-owner/some-action/v4/some-action-v4.pom")
+                    // Then
+                    // This assertion represents an undesired behavior - it should retry generating the binding
+                    // and return the actual POM.
+                    // TODO fix in https://github.com/typesafegithub/github-workflows-kt/issues/1938
+                    response2.status shouldBe HttpStatusCode.InternalServerError
+
+                    // This assertion represents an undesired behavior - it should call the binding generation twice,
+                    // first with exception, and then when it's successful.
+                    // TODO fix in https://github.com/typesafegithub/github-workflows-kt/issues/1938
+                    verify(exactly = 1) { mockBuildVersionArtifacts(any()) }
                 }
             }
         }
