835f4eb3a1ff0bb2dd86e2a6e6f830bb7291ab3d
Piotr Krzeminski <git@krzeminski.it>
2025-06-23 10:04:46 +0200

    Add test

--- github-workflows-kt/src/test/kotlin/io/github/typesafegithub/workflows/domain/StepTest.kt
+++ github-workflows-kt/src/test/kotlin/io/github/typesafegithub/workflows/domain/StepTest.kt
@@ -2,6 +2,8 @@ package io.github.typesafegithub.workflows.domain
 
 import io.github.typesafegithub.workflows.actions.actions.Checkout
 import io.github.typesafegithub.workflows.domain.AbstractResult.Status
+import io.github.typesafegithub.workflows.domain.actions.Action
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
@@ -30,5 +32,32 @@ class StepTest :
         test("step.outputs") {
             val step0: Step<*> = CommandStep(id = "step-0", command = "ls")
             step0.outputs["foo"] shouldBe "steps.step-0.outputs.foo"
+        }
+
+        test("incompatible library version with the binding") {
+            // Given
+            val incompatibleAction = object : Action<Action.Outputs>() {
+                override fun isCompatibleWithLibraryVersion(libraryVersion: String): Boolean =
+                    false
+
+                override fun toYamlArguments(): LinkedHashMap<String, String> =
+                    throw NotImplementedError("Irrelevant for this test")
+
+                override fun buildOutputObject(stepId: String): Outputs =
+                    Outputs(stepId)
+
+                override val usesString: String
+                    get() = throw NotImplementedError("Irrelevant for this test")
+            }
+
+            // When
+            shouldThrow<AssertionError> {
+                ActionStep(
+                    id = "whatever",
+                    action = incompatibleAction,
+                )
+            }.also {
+                it.message shouldBe  "This version of the library is not compatible with the provided action binding!"
+            }
         }
     })
