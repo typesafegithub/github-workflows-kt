817493b07a7f1493f2c16271fbbf84223cdc29d6
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2025-06-25 09:12:07 +0200

    chore: remove workaround with pinning kotlinx-io-core (#1998)
    Closes
    https://github.com/typesafegithub/github-workflows-kt/issues/1832.
    
    We can now remove it since the project started using sufficiently new
    versions of the involved libraries.

--- shared-internal/build.gradle.kts
+++ shared-internal/build.gradle.kts
@@ -21,15 +21,6 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging:7.0.7")
     implementation("com.auth0:java-jwt:4.5.0")
     implementation("org.kohsuke:github-api:1.327")
-    
-    // It's a workaround for a problem with Kotlin Scripting, and how it resolves
-    // conflicting versions: https://youtrack.jetbrains.com/issue/KT-69145
-    // As of adding it, ktor-serialization-kotlinx-json depends on kotlinx-io-core:0.4.0,
-    // and because of how Scripting resolves the libraries, 0.4.0 is used in the script,
-    // leading to a runtime failure of not being able to find a class or a function.
-    // I'm bumping kotlinx-io to 0.6.0 in kotlinx.serialization here: https://github.com/Kotlin/kotlinx.serialization/pull/2933
-    // Here's a ticket to remember to remove this workaround: https://github.com/typesafegithub/github-workflows-kt/issues/1832
-    runtimeOnly("org.jetbrains.kotlinx:kotlinx-io-core:0.8.0")
 
     testImplementation("io.kotest.extensions:kotest-extensions-mockserver:1.3.0")
     testImplementation("org.slf4j:slf4j-simple:2.0.17")
