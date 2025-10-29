128fbb42eac54202c77ed9ac8977565620963b39
Piotr Krzeminski <git@krzeminski.it>
2025-10-29 22:23:14 +0100

    Rename to intendedVersion

--- github-workflows-kt/src/main/kotlin/io/github/typesafegithub/workflows/domain/actions/Action.kt
+++ github-workflows-kt/src/main/kotlin/io/github/typesafegithub/workflows/domain/actions/Action.kt
@@ -25,7 +25,12 @@ public abstract class RegularAction<out OUTPUTS : Outputs>(
     public open val actionOwner: String,
     public open val actionName: String,
     public open val actionVersion: String,
-    public open val comment: String? = null,
+    /**
+     * Set if [actionVersion] is set to a commit hash. Then, [intendedVersion] should be set to the version of the
+     * action corresponding to the commit hash. Thanks to this, the intended version can be used in the resulting
+     * workflow YAML, as a hint for humans and automation tools related to version management.
+     */
+    public open val intendedVersion: String? = null,
 ) : Action<OUTPUTS>() {
     public constructor(
         actionOwner: String,
