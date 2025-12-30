b76a44557b44561f294accc55850f1f004555cc6
Piotr Krzeminski <git@krzeminski.it>
2025-12-30 14:38:08 +0100

    Remove stale comment

--- jit-binding-server/src/main/kotlin/io/github/typesafegithub/workflows/jitbindingserver/ArtifactRoutes.kt
+++ jit-binding-server/src/main/kotlin/io/github/typesafegithub/workflows/jitbindingserver/ArtifactRoutes.kt
@@ -87,9 +87,6 @@ private fun Route.getArtifact(
     refresh: Boolean,
 ) {
     get {
-        // To fix: there'a problem with caching the artifacts under certain keys,
-        //   and parameters.extractActionCoords returning certain (different) ActionCoords.
-        //   Because of this problem, the artifact that uses commit pinning is now not found.
         val bindingArtifacts = call.toBindingArtifacts(refresh, bindingsCache) ?: return@get call.respondNotFound()
 
         if (refresh && !deliverOnRefreshRoute) return@get call.respondText(text = "OK")
